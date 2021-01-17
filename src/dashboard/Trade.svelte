e088c656715edf505bda98513c7091050da74930
thedavidmeister <thedavidmeister@gmail.com>
2021-01-17 13:35:05 +0400

    basic routing

---
+++ src/dashboard/Trade.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let id = ""
+</script>
+
+<div>
+  ID {id}
+</div>
---
+++ src/nav/Nav.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Router, Link, Route } from 'svelte-routing'
+  import Trade from '../dashboard/Trade.svelte'
+
+  export let url = ""
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/trade/pacific-rim-uprising">Pacific Rim Uprising</Link>
+    <Link to="/trade/top-gun">The Top Gun Collection</Link>
+  </nav>
+  <div>
+    <Route path="/trade/:id" let:params>
+      <Trade id="{params.id}" />
+    </Route>
+  </div>
+</Router>
