09a92a335fdca1799338c186fdc73d2b8ec76c40
David Meister <thedavidmeister@gmail.com>
2021-02-11 15:18:24 +0400

    initialize create the pool and gradual weights

--- contracts/TrustPool.sol
+++ contracts/TrustPool.sol
@@ -12,6 +12,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { Constants } from "./libraries/Constants.sol";
 
+import { IBPool } from "./configurable-rights-pool/contracts/IBFactory.sol";
 import { RightsManager } from "./configurable-rights-pool/libraries/RightsManager.sol";
 import { BalancerConstants } from "./configurable-rights-pool/libraries/BalancerConstants.sol";
 import { ConfigurableRightsPool } from "./configurable-rights-pool/contracts/ConfigurableRightsPool.sol";
@@ -19,12 +20,50 @@ import { CRPFactory } from "./configurable-rights-pool/contracts/CRPFactory.sol"
 
 contract TrustPool is Initable {
 
-    bool[] rights;
-    address[] pool_addresses;
-    uint256[] pool_amounts;
-    uint256[] pool_weights;
+    // Everything publicly visible because we have nothing to hide.
 
-    ConfigurableRightsPool public crp_factory;
+    // The rights fo the TrustPool.
+    bool[] public rights;
+
+    // The addresses in the TrustPool, as [reserve, token].
+    address[] public pool_addresses;
+
+    // The amounts of each token at initialization as [reserve_amount, token_amount].
+    uint256[] public pool_amounts;
+
+    // The starting weights of the pool as [min, token/reserve].
+    uint256[] public start_weights;
+
+    // The target weights of the pool, as the inverse of the start weights.
+    uint256[] public target_weights;
+
+    // The start block is set as the block number that init is called.
+    uint256 public start_block;
+
+    // The unlock block is set in the constructor and determines:
+    // - The redeemer unlock
+    // - The ability to withdraw pool tokens
+    // - The auction schedule
+    uint256 public unlock_block;
+
+    address public reserve;
+    address public token;
+    uint256 public reserve_amount;
+
+    ConfigurableRightsPool public crp;
+    IBPool public pool;
+
+    constructor (
+        address _reserve,
+        address _token,
+        uint256 _reserve_amount,
+        uint256 _unlock_block
+    ) public {
+        reserve = _reserve;
+        token = _token;
+        unlock_block = _unlock_block;
+        reserve_amount = _reserve_amount;
+    }
 
     function init_rights() private onlyNotInit returns (RightsManager.Rights memory) {
         // Pause
@@ -87,7 +126,7 @@ contract TrustPool is Initable {
         // The reserve weight is as small as we can make it.
         // The goal is to distribute tokens.
         console.log("Init pool weights: reserve: %s", BalancerConstants.MIN_WEIGHT);
-        pool_weights.push(BalancerConstants.MIN_WEIGHT);
+        start_weights.push(BalancerConstants.MIN_WEIGHT);
 
         // The token weight is the ratio of pooled reserve to token supply.
         // Balancer hard caps the combined weight at 50.
@@ -96,56 +135,90 @@ contract TrustPool is Initable {
         uint256 _headroom = BalancerConstants.BONE;
         uint256 _desired_token_weight = SafeMath.mul(
             SafeMath.div(pool_amounts[1], pool_amounts[0]),
-            pool_weights[0]
+            start_weights[0]
         );
         uint256 _achievable_token_weight = Math.min(
             SafeMath.sub(BalancerConstants.MAX_WEIGHT, _headroom),
             _desired_token_weight
         );
         console.log("Init pool weights: token: %s", _achievable_token_weight);
-        pool_weights.push(_achievable_token_weight);
+        start_weights.push(_achievable_token_weight);
 
-        return pool_weights;
+        // Target weights are the theoretical endpoint of updating gradually.
+        // We simply flip the starting ratios because why not?
+        target_weights.push(start_weights[1]);
+        target_weights.push(start_weights[0]);
+        require(target_weights[0] == start_weights[1], "ERR_TARGET_WEIGHT_0");
+        require(target_weights[1] == start_weights[0], "ERR_TARGET_WEIGHT_1");
+
+        return start_weights;
     }
 
-    function init_pool_fee () private onlyNotInit returns (uint256) {
+    function init_pool_fee () private view onlyNotInit returns (uint256) {
         console.log("TrustPool: Init pool fee: %s", BalancerConstants.MIN_FEE);
         return BalancerConstants.MIN_FEE;
     }
 
-    function init_pool_params(address _reserve_token, address _token, uint256 _reserve_token_amount) private onlyNotInit returns (ConfigurableRightsPool.PoolParams memory) {
-        console.log("TrustPool: Init params: reserve: %s", _reserve_token);
+    function init_pool_params(address _reserve, address _token, uint256 _reserve_amount) private onlyNotInit returns (ConfigurableRightsPool.PoolParams memory) {
+        console.log("TrustPool: Init params: reserve: %s", _reserve);
         console.log("TrustPool: Init params: token: %s", _token);
-        console.log("TrustPool: Init params: reserve amount: %s", _reserve_token_amount);
+        console.log("TrustPool: Init params: reserve amount: %s", _reserve_amount);
         return ConfigurableRightsPool.PoolParams(
             "TrustPool",
             "The Trust Pool",
-            init_pool_addresses(_reserve_token, _token),
-            init_pool_amounts(_reserve_token_amount, _token),
+            init_pool_addresses(_reserve, _token),
+            init_pool_amounts(_reserve_amount, _token),
             init_pool_weights(),
             init_pool_fee()
         );
     }
 
-    function init(
-        address _reserve_token,
-        address _token,
-        uint256 _reserve_token_amount
-    ) public withInit {
-        console.log("TrustPool init: reserve: %s", _reserve_token);
-        console.log("TrustPool init: token: %s", _token);
-        console.log("TrustPool init: reserve amount: %s", _reserve_token_amount);
+    function init() public withInit {
+        console.log("TrustPool init: reserve: %s", reserve);
+        console.log("TrustPool init: token: %s", token);
+        console.log("TrustPool init: reserve amount: %s", reserve_amount);
 
-        require(IERC20(_reserve_token).allowance(msg.sender, address(this)) == _reserve_token_amount, "ERR_RESERVE_ALLOWANCE");
-        require(IERC20(_token).allowance(msg.sender, address(this)) == IERC20(_token).totalSupply(), "ERR_TOKEN_ALLOWANCE");
+        // Ensure the caller set their allowances correctly.
+        require(IERC20(reserve).allowance(msg.sender, address(this)) == reserve_amount, "ERR_RESERVE_ALLOWANCE");
+        require(IERC20(token).allowance(msg.sender, address(this)) == IERC20(token).totalSupply(), "ERR_TOKEN_ALLOWANCE");
 
-        IERC20(_token).transferFrom(msg.sender, address(this), IERC20(_token).totalSupply());
-        require(IERC20(_token).balanceOf(address(this)) == IERC20(_token).totalSupply(), "ERR_TOKEN_TRANSFER");
+        // Take full ownership of the token supply.
+        IERC20(token).transferFrom(msg.sender, address(this), IERC20(token).totalSupply());
+        require(IERC20(token).balanceOf(address(this)) == IERC20(token).totalSupply(), "ERR_TOKEN_TRANSFER");
 
-        crp_factory = CRPFactory(Constants.CRPFactory).newCrp(
+        // Take ownership of the allocated reserves.
+        IERC20(reserve).transferFrom(msg.sender, address(this), reserve_amount);
+        require(IERC20(reserve).balanceOf(address(this)) == reserve_amount, "ERR_RESERVE_TRANSFER");
+
+        // Build a CRPFactory.
+        crp = CRPFactory(Constants.CRPFactory).newCrp(
             Constants.BFactory,
-            init_pool_params(_reserve_token, _token, _reserve_token_amount),
+            init_pool_params(reserve, token, reserve_amount),
             init_rights()
+        );
+
+        // Mint the pool token.
+        // As small as possible because we don't trade/distribute/divide the pool tokens.
+        IERC20(token).approve(address(crp), IERC20(token).totalSupply());
+        IERC20(reserve).approve(address(crp), reserve_amount);
+        ConfigurableRightsPool(crp).createPool(
+            BalancerConstants.MIN_POOL_SUPPLY,
+            // No minimum weight change period.
+            0,
+            // No time lock (we handle our own locks in the trust).
+            0
+        );
+        pool = crp.bPool();
+
+        // Kick off the auction!
+        start_block = block.number;
+        crp.updateWeightsGradually(
+            // Flip the weights
+            target_weights,
+            // From now
+            start_block,
+            // Until unlock
+            unlock_block
         );
     }
 
--- test/TrustPool.sol.ts
+++ test/TrustPool.sol.ts
@@ -2,6 +2,7 @@ import * as Util from './Util'
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import type { ReserveToken } from "../typechain/ReserveToken"
+import type { TrustToken } from "../typechain/TrustToken"
 import type { TrustPool } from "../typechain/TrustPool"
 import { ethers } from 'hardhat';
 
@@ -16,41 +17,51 @@ describe("TrustPool", async function() {
 
         const reserve = await Util.basicDeploy("ReserveToken", {}) as ReserveToken
 
-        const trustPool = await Util.basicDeploy("TrustPool", {
-            // Mainnet rights manager.
-            "RightsManager": '0x0F811b1AF2B6B447B008eFF31eCceeE5A0b1d842'
-        }) as TrustPool
-
         const tokenFactory = await ethers.getContractFactory(
             "TrustToken"
         )
 
         // Ten thousand to the 18
         const tokenSupply = ethers.BigNumber.from('10000' + Util.eighteenZeros)
-        const token = await tokenFactory.deploy(tokenSupply, "Some token", "TKN")
+        const token = await tokenFactory.deploy(tokenSupply, "Some token", "TKN") as TrustToken
 
         await token.deployed()
 
-        // Send aaaalll the tokens to the trust pool
-        // console.log(`Transfer from ${signers[0].address} to ${trustPool.address} for ${tokenSupply}`)
-        await token.approve(trustPool.address, (await token.totalSupply()))
-        // const allowance = await token.allowance(signers[0].address, trustPool.address)
-        // console.log(allowance)
-        // await token.transferFrom(signers[0].address, trustPool.address, (await token.totalSupply()))
-
         // One thousand to the 18
         const reserveSupply = ethers.BigNumber.from('1000' + Util.eighteenZeros)
-        console.log(`Increase allowance by ${reserve.address} for ${trustPool.address} of ${reserveSupply}`)
-        await reserve.approve(trustPool.address, reserveSupply)
 
-        await trustPool.init(
-            reserve.address,
-            token.address,
-            reserveSupply
+        const trustPoolFactory = await ethers.getContractFactory(
+            "TrustPool",
+            {
+                libraries: {
+                    // Mainnet rights manager.
+                    "RightsManager": '0x0F811b1AF2B6B447B008eFF31eCceeE5A0b1d842'
+                }
+            }
         )
 
-        const trustPoolCrpFactory = await trustPool.crp_factory()
-        console.log(`Initialized crpFactory: ${trustPoolCrpFactory}`)
+        const unlockBlock = 11833335 + 50
+
+        const trustPool = await trustPoolFactory.deploy(
+            reserve.address,
+            token.address,
+            reserveSupply,
+            unlockBlock
+        ) as TrustPool
+
+        await trustPool.deployed()
+
+        // Init will revert if these approvals are not exact.
+        await token.approve(trustPool.address, (await token.totalSupply()))
+        await reserve.approve(trustPool.address, reserveSupply)
+
+        await trustPool.init()
+
+        const trustPoolCrp = await trustPool.crp()
+        console.log(`Initialized crp: ${trustPoolCrp}`)
+
+        const trustPoolPool = await trustPool.pool()
+        console.log(`Initialized pool: ${trustPoolPool}`)
 
     })
 })
