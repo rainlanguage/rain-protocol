0715624457fc3a6ba33ff265d9d89d37fa280c43
thedavidmeister <thedavidmeister@gmail.com>
2021-03-09 02:04:02 +0400

    track statuses on tvk prestige

--- contracts/TVKPrestige.sol
+++ contracts/TVKPrestige.sol
@@ -9,6 +9,8 @@ import "./IPrestige.sol";
 contract TVKPrestige is IPrestige {
     IERC20 public constant tvk = IERC20(0xd084B83C305daFD76AE3E1b4E1F1fe2eCcCb3988);
 
+    mapping (address => Status) public statuses;
+
     uint256 public constant common = uint256(0);
     uint256 public constant uncommon = uint256(10 ** 18);
     uint256 public constant rare = uint256(20 ** 18);
@@ -23,9 +25,7 @@ contract TVKPrestige is IPrestige {
     }
 
     function set_status(address _account, Status _status) external override {
-        emit StatusChange(_account, this.status(_account), _status);
-
-        uint256 _current = tvk.balanceOf(_account);
+        uint256 _current = levels()[uint256(this.status(_account))];
         // Status enum casts to level index.
         uint256 _new_limit = levels()[uint256(_status)];
 
@@ -43,24 +43,12 @@ contract TVKPrestige is IPrestige {
             ));
         }
 
-        require(tvk.balanceOf(_account) == _new_limit, "ERR_STATUS_FUND");
+        statuses[_account] = _status;
+
+        emit StatusChange(_account, this.status(_account), _status);
     }
 
     function status(address _account) external override view returns (Status) {
-        uint256 balance = tvk.balanceOf(_account);
-        uint256[6] memory _levels = levels();
-        uint8 _i;
-        for (_i = 0; _i < 6; _i++) {
-            // Someone could grief a user by sending TVK to this account
-            // if we require exact balances on lookup.
-            if (_levels[_i] > balance) {
-                return Status(_i-1);
-            }
-            else if (_levels[_i] == balance) {
-                return Status(_i);
-            }
-        }
-        // We checked every level and balance larger than all.
-        return Status(_i);
+        return statuses[_account];
     }
 }
