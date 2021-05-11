d17752af05c2791b6b7a4ff326c334885d60ba2c
thedavidmeister <thedavidmeister@gmail.com>
2021-05-12 02:25:31 +0400

    docs

--- contracts/Prestige.sol
+++ contracts/Prestige.sol
@@ -8,9 +8,11 @@ import { PrestigeUtil } from "./PrestigeUtil.sol";
 contract Prestige is IPrestige {
     mapping(address => uint256) public statuses;
 
-    /// Return status report
-    /// @param account - Account to be reported on.
-    /// @return uint256 the block number that corresponds to the current status report.
+    /**
+     * Implements IPrestige.
+     *
+     * Either fetch the report from storage or return UNINITIALIZED.
+     */
     function statusReport(address account)
         public
         virtual
@@ -22,6 +24,14 @@ contract Prestige is IPrestige {
         return report == 0 ? PrestigeUtil.UNINITIALIZED : report;
     }
 
+    /**
+     * Implements IPrestige.
+     *
+     * Errors if the user attempts to return to the NIL status.
+     * Updates the status report from `statusReport` using default `PrestigeUtil` logic.
+     * Calls `_afterSetStatus` that inheriting contracts SHOULD override to enforce status requirements.
+     * Emits `StatusChange` event.
+     */
     function setStatus(address account, Status newStatus, bytes memory data) external virtual override {
         // The user must move to at least COPPER.
         // The NIL status is reserved for users that have never interacted with the contract.
@@ -47,5 +57,16 @@ contract Prestige is IPrestige {
         emit StatusChange(account, [currentStatus, newStatus]);
     }
 
+    /**
+     * Inheriting contracts SHOULD override this to enforce status requirements.
+     *
+     * All the internal accounting and state changes are complete at this point.
+     * Use `require` to enforce additional requirements for status changes.
+     *
+     * @param account The account with the new status.
+     * @param oldStatus The status the account had before this update.
+     * @param newStatus The status the account will have after this update.
+     * @param data Additional arbitrary data to inform status update requirements.
+     */
     function _afterSetStatus(address account, Status oldStatus, Status newStatus, bytes memory data) internal virtual { } // solhint-disable-line no-empty-blocks
 }
