ecc44498d4fc28c4ecce0e38f99a7d6a865ef692
thedavidmeister <thedavidmeister@gmail.com>
2021-06-21 15:34:11 +0400

    lint docs

--- contracts/tier/AlwaysTier.sol
+++ contracts/tier/AlwaysTier.sol
@@ -4,8 +4,16 @@ pragma solidity ^0.6.12;
 
 import "./ReadOnlyTier.sol";
 
+/// @title AlwaysTier
+///
+/// `AlwaysTier` is intended as a primitive for combining tier contracts.
+///
+/// As the name implies:
+/// - `AlwaysTier` is `ReadOnlyTier` and so can never call `setTier`.
+/// - `report` is always `0x00000000` for every tier and every address.
 contract AlwaysTier is ReadOnlyTier {
-    // Users are always AlwaysTier for every tier.
+    /// Every address is always every tier.
+    /// @inheritdoc ITier
     function report(address) public override view returns (uint256) {
         return 0;
     }
