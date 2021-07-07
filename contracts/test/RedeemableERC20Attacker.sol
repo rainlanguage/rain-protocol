da0fbee5d14c9366d28ddba750bf020fa00acbcc
marcusnewton <marcus.newton@hotmail.com>
2021-07-07 18:09:10 +1000

    WIP OZ ERC20 attacker _beforeTokenTransfer

--- contracts/test/RedeemableERC20Attacker.sol
+++ contracts/test/RedeemableERC20Attacker.sol
@@ -3,13 +3,14 @@
 pragma solidity ^0.6.12;
 
 import {RedeemableERC20} from "../RedeemableERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
-
-contract RedeemableERC20Attacker {
-    uint private constant BONE = 10**18;
+contract RedeemableERC20Attacker is ERC20 {
+    uint256 private constant BONE = 10**18;
     RedeemableERC20 public token;
 
-    constructor(address victim_) public {
+    constructor(address victim_) public ERC20("Attacker", "ATK") {
         token = RedeemableERC20(victim_);
     }
 
@@ -17,11 +18,17 @@ contract RedeemableERC20Attacker {
         selfdestruct(msg.sender);
     }
 
-    function attack() external payable {
+    function attack() external {
         token.senderRedeem(BONE);
     }
 
-    fallback() external payable {
+    function _beforeTokenTransfer(
+        address sender_,
+        address receiver_,
+        uint256 amount_
+    ) internal virtual override {
+        super._beforeTokenTransfer(sender_, receiver_, amount_);
+        console.log("_beforeTokenTransfer triggered");
         token.senderRedeem(BONE);
     }
 }
