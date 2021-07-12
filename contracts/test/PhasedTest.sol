cfc588ae60365c5325274fd8a24b5f40114d4913
marcusnewton <marcus.newton@hotmail.com>
2021-07-12 19:22:36 +1000

    add phased tests

--- contracts/test/PhasedTest.sol
+++ contracts/test/PhasedTest.sol
@@ -2,7 +2,39 @@
 
 pragma solidity ^0.6.12;
 
-import { Phased } from "../Phased.sol";
+import { Phase, Phased } from "../Phased.sol";
 
 /// Empty contract for tests enumerating behaviour of the modifiers.
-contract PhasedTest is Phased { } //solhint-disable-line no-empty-blocks
+contract PhasedTest is Phased {
+    /// Custom variable for testing _beforeScheduleNextPhase hook
+    bool public hookCondition = true;
+
+    function testScheduleNextPhase(uint32 nextPhaseBlock_) external {
+        scheduleNextPhase(nextPhaseBlock_);
+    }
+
+    /// This function wraps `onlyPhase` modifier, passing phase directly into modifier argument.
+    /// Returns true only if current phase matches phase passed in.
+    function runsOnlyPhase(Phase phase_) external view onlyPhase(phase_) returns(bool)
+    {
+        return true;
+    }
+
+    /// This function wraps `onlyAtLeastPhase` modifier, passing phase directly into modifier argument.
+    /// Returns true only if current phase is equal to or greater than phase passed in.
+    function runsOnlyAtLeastPhase(Phase phase_) external view onlyAtLeastPhase(phase_) returns(bool)
+    {
+        return true;
+    }
+
+    /// Custom function for testing _beforeScheduleNextPhase hook
+    /// Toggles the hook condition
+    function toggleHookCondition() external {
+        hookCondition = !hookCondition;
+    }
+
+    function _beforeScheduleNextPhase(uint32 nextPhaseBlock_) internal virtual override {
+        require(hookCondition, "HOOK_CONDITION");
+        super._beforeScheduleNextPhase(nextPhaseBlock_);
+    }
+}
