6542845c8d4b44599f7f986fea3726ca53e1acdf
thedavidmeister <thedavidmeister@gmail.com>
2021-08-02 18:08:14 +0400

    solhint

--- contracts/test/PrestigeTest.sol
+++ contracts/test/PrestigeTest.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.6.12;
 
 import { Prestige } from "../tv-prestige/contracts/Prestige.sol";
 
-/// Empty contract for tests enumerating behaviour of the modifiers at each status level.
+/// Empty contract for tests enumerating behaviour of the modifiers at each
+/// status level.
 contract PrestigeTest is Prestige { } //solhint-disable-line no-empty-blocks
