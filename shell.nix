3bfc51fde774c051bfdfbb7500c6f82ef9e42a3d
thedavidmeister <thedavidmeister@gmail.com>
2021-09-28 19:37:55 +0400

    remove slither hack patches

--- shell.nix
+++ shell.nix
@@ -31,16 +31,6 @@ let
   '';
 
   security-check = pkgs.writeShellScriptBin "security-check" ''
-    # Slither does not like there being two IERC20.
-    # One is from Balancer the other is from Open Zeppelin.
-    # This patch swaps all the Balancer IERC20 imports with an Open Zeppelin IERC20 import.
-    # patch -p1 < slither-hack-balancer-ierc20.patch
-
-    # Balancer has PoolParams struct defined inside a contract which slither does not like.
-    # This patch moves PoolParams outside the contract and upates import references to it.
-    # patch -p1 < slither-hack-balancer-pool-params.patch
-    # patch -p1 < slither-hack-local-pool-params.patch
-
     # Workaround a slither bug due to stale compiled artifacts.
     # https://github.com/crytic/slither/issues/860
     rm -rf artifacts
@@ -60,11 +50,6 @@ let
     # - The test contracts that only exist so the test harness can drive unit tests and will never be deployed
     # - Open Zeppelin contracts
     slither . --npx-disable --filter-paths="contracts/test" --exclude-dependencies
-
-    # Rollback all the slither specific patches.
-    # patch -R -p1 < slither-hack-balancer-ierc20.patch
-    # patch -R -p1 < slither-hack-balancer-pool-params.patch
-    # patch -R -p1 < slither-hack-local-pool-params.patch
   '';
 
   ci-test = pkgs.writeShellScriptBin "ci-test" ''
