4907b788aaac89284843d58e01d2a90c53e15b62
thedavidmeister <thedavidmeister@gmail.com>
2021-09-30 17:37:21 +0400

    lint

--- contracts/tier/VerifyTier.sol
+++ contracts/tier/VerifyTier.sol
@@ -8,11 +8,21 @@ import "./ReadOnlyTier.sol";
 import { State, Status, Verify } from "../verify/Verify.sol";
 import "../libraries/TierUtil.sol";
 
+/// @title VerifyTier
+///
+/// @dev A contract that is `VerifyTier` expects to derive tiers from the time
+/// the account was approved by the underlying `Verify` contract. The approval
+/// block numbers defer to `State.since` returned from `Verify.state`.
 contract VerifyTier is ReadOnlyTier {
     Verify public immutable verify;
+
+    /// Sets the `verify` contract immutably.
     constructor(Verify verify_) public {
         verify = verify_;
     }
+
+    ///
+    /// @inheritdoc ITier
     function report(address account_) public override view returns (uint256) {
         State memory state_ = verify.state(account_);
         if (state_.status == Status.Approved) {
