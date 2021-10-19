94d32096fd4b887dd2a9c6d7e21d1769f4da164f
marcusnewton <marcus.newton@hotmail.com>
2021-10-19 14:48:39 +1100

    init test folder

---
+++ test/Compiler/RainCompiler.sol.ts
@@ -0,0 +1,10 @@
+import * as Util from "../Util";
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import { ethers } from "hardhat";
+import type { RainCompiler } from "../../typechain/RainCompiler";
+
+chai.use(solidity);
+const { expect, assert } = chai;
+
+describe("RainCompiler", async function () {});
