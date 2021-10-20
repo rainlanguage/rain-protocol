ad6f5a3612e33c09ebb7a0c4d6d09c3807765ba5
marcusnewton <marcus.newton@hotmail.com>
2021-10-20 15:55:57 +1100

    remove duplicate code

--- test/Tier/CombineTier.sol.ts
+++ test/Tier/CombineTier.sol.ts
@@ -133,51 +133,7 @@ describe("CombineTier", async function () {
       source
     )) as CombineTier;
 
-    // console.log(
-    `constants
-    MAX_COMPILED_SOURCE_LENGTH  ${await combineTier.MAX_COMPILED_SOURCE_LENGTH()}
-    LIT_SIZE_BYTES              ${await combineTier.LIT_SIZE_BYTES()}
-
-    OPCODE_END                  ${await combineTier.OPCODE_END()}
-    OPCODE_LIT                  ${await combineTier.OPCODE_LIT()}
-    OPCODE_ARG                  ${await combineTier.OPCODE_ARG()}
-
-    OPCODE_VAL                  ${await combineTier.OPCODE_VAL()}
-    OPCODE_CALL                 ${await combineTier.OPCODE_CALL()}
-
-    OPCODE_BLOCK_NUMBER         ${await combineTier.OPCODE_BLOCK_NUMBER()}
-
-    OPCODE_RESERVED_MAX         ${await combineTier.OPCODE_RESERVED_MAX()}
-
-    OPCODE_ACCOUNT              ${await combineTier.OPCODE_ACCOUNT()}
-    OPCODE_REPORT               ${await combineTier.OPCODE_REPORT()}
-
-    OPCODE_AND_OLD              ${await combineTier.OPCODE_AND_OLD()}
-    OPCODE_AND_NEW              ${await combineTier.OPCODE_AND_NEW()}
-    OPCODE_AND_LEFT             ${await combineTier.OPCODE_AND_LEFT()}
-    OPCODE_OR_OLD               ${await combineTier.OPCODE_OR_OLD()}
-    OPCODE_OR_NEW               ${await combineTier.OPCODE_OR_NEW()}
-    OPCODE_OR_LEFT              ${await combineTier.OPCODE_OR_LEFT()}
-
-    val0                        ${await combineTier.val0()}
-    val1                        ${await combineTier.val1()}
-    val2                        ${await combineTier.val2()}
-    val3                        ${await combineTier.val3()}
-    val4                        ${await combineTier.val4()}
-    val5                        ${await combineTier.val5()}
-    val6                        ${await combineTier.val6()}
-    val7                        ${await combineTier.val7()}
-    val8                        ${await combineTier.val8()}
-    val9                        ${await combineTier.val9()}
-    val10                       ${await combineTier.val10()}
-    val11                       ${await combineTier.val11()}
-    val12                       ${await combineTier.val12()}
-
-    source0                     ${await combineTier.source0()}
-    source1                     ${await combineTier.source1()}
-    source2                     ${await combineTier.source2()}
-    source3                     ${await combineTier.source3()}`;
-    //)
+    console.log(await getConstants(combineTier));
   });
 });
 
