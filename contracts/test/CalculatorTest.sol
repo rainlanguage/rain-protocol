4d1cb437fec1603abb938522a837b0bb8055d5c7
marcusnewton <marcus.newton@hotmail.com>
2021-10-25 17:01:56 +1100

    update DIV to calculate denominator before performing division

--- contracts/test/CalculatorTest.sol
+++ contracts/test/CalculatorTest.sol
@@ -67,16 +67,18 @@ contract CalculatorTest is RainCompiler {
             stack_.index++;
         } else if (op_.code == OPCODE_DIV) {
             stack_.index -= op_.val;
-            // Set initial value as first number.
-            uint256 accumulator_ = stack_.vals[stack_.index + op_.val - 1];
-            for (uint256 a_ = 0; a_ < op_.val - 1; a_++) {
-                // Iterate backwards through inputs, dividing the current
-                // value by each one, being careful not to divide by the first
-                // number again.
-                accumulator_ = accumulator_
-                    .div(stack_.vals[stack_.index + a_]);
+            // Set numerator value as first number.
+            uint256 numerator_ = stack_.vals[stack_.index + op_.val - 1];
+            // Set initial denominator value as second number.
+            uint256 denominator_ = stack_.vals[stack_.index + op_.val - 2];
+            for (uint256 a_ = 0; a_ < op_.val - 2; a_++) {
+                // Iterate backwards through inputs, calculating the total
+                // denominator, being careful not to multiply by the initial
+                // denominator value again.
+                denominator_ = denominator_
+                    .mul(stack_.vals[stack_.index + a_]);
             }
-            stack_.vals[stack_.index] = accumulator_;
+            stack_.vals[stack_.index] = numerator_.div(denominator_);
             stack_.index++;
         } else if (op_.code == OPCODE_MOD) {
             stack_.index -= op_.val;
