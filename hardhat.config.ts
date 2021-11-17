ea9238a040265019a29b1eab357ed80315df90e2
NanezX <victor.nanezj@gmail.com>
2021-11-17 17:12:18 -0400

    Prettier ran

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
@@ -41,171 +41,171 @@ const config: HardhatUserConfig = {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/ConfigurableRightsPoolImports.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/PhasedScheduleTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/PhasedTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/RedeemableERC20Reentrant.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/ReserveToken.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/ReserveTokenTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/SeedERC20ForceSendEther.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/SeedERC20Reentrant.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/TierByConstructionClaimTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/TierByConstructionTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/TierUtilTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/TrustReentrant.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
+            runs: 100000,
+          },
+        },
       },
       "contracts/test/ValueTierTest.sol": {
         version: "0.6.12",
         settings: {
           metadata: {
-            useLiteralContent: true
+            useLiteralContent: true,
           },
           optimizer: {
             enabled: true,
-            runs: 100000
-          }
-        }
-      }
-    }
+            runs: 100000,
+          },
+        },
+      },
+    },
   },
 };
 export default config;
