292c0dbb5cdb07a267b0c1c3ebd53307ea14fcf8
marcusnewton <marcus.newton@hotmail.com>
2021-11-25 19:26:25 +1100

    fix merge conflict

--- contracts/trust/Trust.sol
+++ contracts/trust/Trust.sol
@@ -290,8 +290,6 @@ contract Trust is ReentrancyGuard {
     using SafeERC20 for RedeemableERC20;
 
     BPoolFeeEscrow public immutable bPoolFeeEscrow;
-    // FIXME: Cannot make seedERC20Config struct (or strings) `immutable`
-    ERC20Config public seedERC20Config;
 
     /// Anyone can emit a `Notice`.
     /// This is open ended content related to the `Trust`.
@@ -394,7 +392,6 @@ contract Trust is ReentrancyGuard {
         seedERC20Factory = config_.seedERC20Factory;
         successBalance = successBalance_;
         bPoolFeeEscrow = config_.bPoolFeeEscrow;
-        seedERC20Config = config_.seedERC20Config;
 
         RedeemableERC20 redeemableERC20_ = RedeemableERC20(
             trustRedeemableERC20Config_.redeemableERC20Factory
