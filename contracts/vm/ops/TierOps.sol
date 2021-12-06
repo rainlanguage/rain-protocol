d9cc518ccc7fc68d98da0f47bfc9d83e5d08ae29
thedavidmeister <thedavidmeister@gmail.com>
2021-12-06 17:44:06 +0400

    lint

--- contracts/vm/ops/TierOps.sol
+++ contracts/vm/ops/TierOps.sol
@@ -56,7 +56,7 @@ library TierOps {
         }
         else if (op_.code == uint8(Ops.updateBlocksForTierRange)) {
             ITier.Tier startTier_ = ITier.Tier(op_.val & 0x0f);
-            ITier.Tier endTier_ = ITier.Tier((op_.val & 0xf0) >> 4);
+            ITier.Tier endTier_ = ITier.Tier((op_.val >> 4) & 0x0f);
             stack_.index -= 2;
             uint256 blockNumber_ = stack_.vals[stack_.index];
             uint256 report_ = stack_.vals[stack_.index + 1];
