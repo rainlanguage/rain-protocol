1699c827be33ee56a71443b73b0848b7a8c3d5e2
marcusnewton <marcus.newton@hotmail.com>
2022-02-09 20:15:52 +1100

    test token balance ops

---
+++ contracts/test/ReserveTokenERC1155.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: CAL
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+// solhint-disable-next-line max-line-length
+import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+/// @title ReserveTokenERC1155
+// Extremely basic ERC1155 implementation for testing purposes.
+contract ReserveTokenERC1155 is ERC1155, ERC1155Burnable {
+    // Stables such as USDT and USDC commonly have 6 decimals.
+    uint256 public constant DECIMALS = 6;
+    // One _billion_ dollars 👷😈.
+    uint256 public constant TOTAL_SUPPLY = 10**(DECIMALS + 9);
+
+    // Incremented token count for use as id for newly minted tokens.
+    uint256 public tokenCount;
+
+    /// Define and mint a erc1155 token.
+    constructor() ERC1155("") {
+        tokenCount = 0;
+        _mint(msg.sender, tokenCount, TOTAL_SUPPLY, "");
+    }
+
+    function decimals() public pure returns (uint8) {
+        return uint8(DECIMALS);
+    }
+
+    function mintNewToken() external {
+        tokenCount++;
+        _mint(msg.sender, tokenCount, TOTAL_SUPPLY, "");
+    }
+}
