b3b0de42799c010ed1fb52bf7535686b876a895e
marcusnewton <marcus.newton@hotmail.com>
2022-02-18 16:24:22 +1100

    test saturating report diff

--- test/VM/TierOpsTest.sol.ts
+++ test/VM/TierOpsTest.sol.ts
@@ -36,19 +36,17 @@ enum Tier {
 }
 
 describe("TierOpsTest", async function () {
-  it("should use saturating sub for diff (does not panic when underflowing)", async () => {
-    throw new Error("Unimplemented");
-  });
-
-  it("should diff reports correctly", async () => {
+  it("should use saturating sub for diff where only some tiers would underflow", async () => {
     this.timeout(0);
 
     const tierOpsFactory = await ethers.getContractFactory("TierOpsTest");
 
     const constants0 = [
-      Util.blockNumbersToReport([0, 0, 0, 0, 0, 7, 8, 9].reverse()),
-      Util.blockNumbersToReport([0, 0, 0, 0, 0, 5, 6, 7].reverse()),
-    ]; // is something fishy going on here?
+      //         0x01000000020000000300000004000000050000000600000007
+      Util.blockNumbersToReport([0, 1, 2, 3, 4, 5, 6, 7].reverse()),
+      // 0x0200000000000000040000000000000006000000000000000800000000
+      Util.blockNumbersToReport([2, 0, 4, 0, 6, 0, 8, 0].reverse()),
+    ];
 
     const vReport0 = op(Opcode.VAL, 0);
     const vReport1 = op(Opcode.VAL, 1);
@@ -67,18 +65,99 @@ describe("TierOpsTest", async function () {
       stackLength: 10,
     })) as TierOpsTest & Contract;
 
-    const result0 = await tierOps0.run(); // 0x470de500000000
+    const result0 = await tierOps0.run();
     const resultHex0 = hexlify(result0);
-    const state0 = await tierOps0.runState();
 
-    console.log({ result: result0, resultHex: resultHex0, state: state0 });
-
-    const expectedResult0 = "0x020000000200000002";
+    const expectedResultHex0 =
+      "0x01000000000000000300000000000000050000000000000007";
 
     assert(
-      resultHex0 === expectedResult0,
+      resultHex0 === expectedResultHex0,
       `wrong report diff
-      expected  ${expectedResult0}
+      expected  ${expectedResultHex0}
+      got       ${resultHex0}`
+    );
+  });
+
+  it("should use saturating sub for diff (does not panic when underflowing, but sets to zero)", async () => {
+    this.timeout(0);
+
+    const tierOpsFactory = await ethers.getContractFactory("TierOpsTest");
+
+    const constants0 = [
+      // 0x01000000020000000300000004000000050000000600000007
+      Util.blockNumbersToReport([0, 1, 2, 3, 4, 5, 6, 7].reverse()),
+      // 0x0200000003000000040000000500000006000000070000000800000009
+      Util.blockNumbersToReport([2, 3, 4, 5, 6, 7, 8, 9].reverse()),
+    ];
+
+    const vReport0 = op(Opcode.VAL, 0);
+    const vReport1 = op(Opcode.VAL, 1);
+
+    // prettier-ignore
+    const source0 = concat([
+        vReport0,
+        vReport1,
+      op(Opcode.SATURATING_DIFF),
+    ]);
+
+    const tierOps0 = (await tierOpsFactory.deploy({
+      sources: [source0],
+      constants: constants0,
+      argumentsLength: 0,
+      stackLength: 10,
+    })) as TierOpsTest & Contract;
+
+    const result0 = await tierOps0.run();
+    const resultHex0 = hexlify(result0);
+
+    assert(
+      result0.isZero(),
+      `wrong report diff
+      expected  ${0x00}
+      got       ${resultHex0}`
+    );
+  });
+
+  it("should diff reports correctly", async () => {
+    this.timeout(0);
+
+    const tierOpsFactory = await ethers.getContractFactory("TierOpsTest");
+
+    const constants0 = [
+      // 0x0200000003000000040000000500000006000000070000000800000009
+      Util.blockNumbersToReport([2, 3, 4, 5, 6, 7, 8, 9].reverse()),
+      // 0x01000000020000000300000004000000050000000600000007
+      Util.blockNumbersToReport([0, 1, 2, 3, 4, 5, 6, 7].reverse()),
+    ];
+
+    const vReport0 = op(Opcode.VAL, 0);
+    const vReport1 = op(Opcode.VAL, 1);
+
+    // prettier-ignore
+    const source0 = concat([
+        vReport0,
+        vReport1,
+      op(Opcode.SATURATING_DIFF),
+    ]);
+
+    const tierOps0 = (await tierOpsFactory.deploy({
+      sources: [source0],
+      constants: constants0,
+      argumentsLength: 0,
+      stackLength: 10,
+    })) as TierOpsTest & Contract;
+
+    const result0 = await tierOps0.run();
+    const resultHex0 = hexlify(result0);
+
+    const expectedResultHex0 =
+      "0x0200000002000000020000000200000002000000020000000200000002";
+
+    assert(
+      resultHex0 === expectedResultHex0,
+      `wrong report diff
+      expected  ${expectedResultHex0}
       got       ${resultHex0}`
     );
   });
