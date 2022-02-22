fcc99b09f7292970a91951c3ce62c9b73fdca97e
thedavidmeister <thedavidmeister@gmail.com>
2022-02-22 12:55:04 +0400

    allow bulk and id notices

--- contracts/noticeboard/NoticeBoard.sol
+++ contracts/noticeboard/NoticeBoard.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.10;
 
+struct Notice {
+    address subject;
+    bytes data;
+}
+
 contract NoticeBoard {
+    uint256 private nextId;
     /// Anyone can emit a `Notice`.
     /// This is open ended content related to the subject.
     /// Some examples:
@@ -10,21 +16,25 @@ contract NoticeBoard {
     /// - Simple onchain voting/signalling
     /// GUIs/tooling/indexers reading this data are expected to know how to
     /// interpret it in context because the `NoticeBoard` contract does not.
-    event Notice(
+    event NewNotice(
         /// The `msg.sender` that emitted the `Notice`.
         address sender,
-        /// The subject of the `Notice`.
-        address subject,
-        /// Opaque binary data for the GUI/tooling/indexer to read.
-        bytes data
+        /// The id of the event.
+        uint256 id,
+        /// The notice data.
+        Notice notice
     );
 
-    /// Anyone can send a notice about the subject.
+    /// Anyone can create notices about some subject.
     /// The notice is opaque bytes. The indexer/GUI is expected to understand
     /// the context to decode/interpret it.
-    /// @param subject_ The subject of the notice.
-    /// @param data_ The data associated with this notice.
-    function sendNotice(address subject_, bytes memory data_) external {
-        emit Notice(msg.sender, subject_, data_);
+    /// @param notices_ All the notices to emit.
+    function createNotices(Notice[] calldata notices_) external {
+        uint256 id_ = nextId;
+        for (uint256 i_ = 0; i_ < notices_.length; i_++) {
+            emit NewNotice(msg.sender, id_, notices_[i_]);
+            id_++;
+        }
+        nextId = id_;
     }
 }
