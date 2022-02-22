764effdb5b38b88ad04d45a10f797199e1fc5d10
marcusnewton <marcus.newton@hotmail.com>
2022-02-22 16:28:24 +1100

    test fixed point math ops

--- test/VM/FixedPointMathsOpsTest.sol.ts
+++ test/VM/FixedPointMathsOpsTest.sol.ts
@@ -32,22 +32,150 @@ describe("FixedPointMathOpsTest", async function () {
     );
   });
 
+  it("should return DECIMALS", async () => {
+    this.timeout(0);
+
+    const constants = [];
+
+    // prettier-ignore
+    const sources = [
+      concat([
+        op(Opcode.DECIMALS)
+      ]),
+    ];
+
+    const tokenOpsTest = (await tokenOpsTestFactory.deploy({
+      sources,
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as FixedPointMathOpsTest & Contract;
+
+    const result0 = await tokenOpsTest.run();
+    const expected0 = 18;
+
+    assert(
+      result0.eq(expected0),
+      `wrong result
+      expected  ${expected0}
+      got       ${result0}`
+    );
+  });
+
+  it("should return ONE", async () => {
+    this.timeout(0);
+
+    const constants = [];
+
+    // prettier-ignore
+    const sources = [
+      concat([
+        op(Opcode.ONE)
+      ]),
+    ];
+
+    const tokenOpsTest = (await tokenOpsTestFactory.deploy({
+      sources,
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as FixedPointMathOpsTest & Contract;
+
+    const result0 = await tokenOpsTest.run();
+    const expected0 = ethers.BigNumber.from(1 + Util.eighteenZeros);
+
+    assert(
+      result0.eq(expected0),
+      `wrong result
+      expected  ${expected0}
+      got       ${result0}`
+    );
+  });
+
+  it("should scale an arbitrary fixed point number DOWN by scale N", async () => {
+    this.timeout(0);
+
+    const value1 = ethers.BigNumber.from(1 + Util.sixZeros);
+    const n = 0xfc; // -4
+
+    const constants = [value1];
+    const v1 = op(Opcode.VAL, 0);
+
+    // prettier-ignore
+    const sources = [
+      concat([
+          v1,
+        op(Opcode.SCALE_BY, n)
+      ]),
+    ];
+
+    const tokenOpsTest = (await tokenOpsTestFactory.deploy({
+      sources,
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as FixedPointMathOpsTest & Contract;
+
+    const result0 = await tokenOpsTest.run();
+    const expected0 = ethers.BigNumber.from(100);
+
+    assert(
+      result0.eq(expected0),
+      `wrong result
+      expected  ${expected0}
+      got       ${result0}`
+    );
+  });
+
+  it("should scale an arbitrary fixed point number UP by scale N", async () => {
+    this.timeout(0);
+
+    const value1 = ethers.BigNumber.from(1 + Util.sixZeros);
+    const n = 0x04; // 4
+
+    const constants = [value1];
+    const v1 = op(Opcode.VAL, 0);
+
+    // prettier-ignore
+    const sources = [
+      concat([
+          v1,
+        op(Opcode.SCALE_BY, n)
+      ]),
+    ];
+
+    const tokenOpsTest = (await tokenOpsTestFactory.deploy({
+      sources,
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as FixedPointMathOpsTest & Contract;
+
+    const result0 = await tokenOpsTest.run();
+    const expected0 = ethers.BigNumber.from(1 + Util.sixZeros + "0000");
+
+    assert(
+      result0.eq(expected0),
+      `wrong result
+      expected  ${expected0}
+      got       ${result0}`
+    );
+  });
+
   it("should scale an 18 OOMs number UP to scale N", async () => {
     this.timeout(0);
 
     const value1 = ethers.BigNumber.from(1 + Util.eighteenZeros);
     const n = 20;
 
-    const constants = [value1, n];
+    const constants = [value1];
     const v1 = op(Opcode.VAL, 0);
-    const vN = op(Opcode.VAL, 1);
 
     // prettier-ignore
     const sources = [
       concat([
           v1,
-          vN,
-        op(Opcode.SCALEN)
+        op(Opcode.SCALEN, n)
       ]),
     ];
 
@@ -75,16 +203,14 @@ describe("FixedPointMathOpsTest", async function () {
     const value1 = ethers.BigNumber.from(1 + Util.eighteenZeros);
     const n = 6;
 
-    const constants = [value1, n];
+    const constants = [value1];
     const v1 = op(Opcode.VAL, 0);
-    const vN = op(Opcode.VAL, 1);
 
     // prettier-ignore
     const sources = [
       concat([
           v1,
-          vN,
-        op(Opcode.SCALEN)
+        op(Opcode.SCALEN, n)
       ]),
     ];
 
