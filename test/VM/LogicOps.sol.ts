0e88734616932d3b8a33ac9a0277a1056744a1a5
marcusnewton <marcus.newton@hotmail.com>
2022-02-23 16:20:47 +1100

    add tests

--- test/VM/LogicOps.sol.ts
+++ test/VM/LogicOps.sol.ts
@@ -26,6 +26,144 @@ const enum Opcode {
 const isTruthy = (vmValue: BigNumber) => vmValue.eq(1);
 
 describe("LogicOps", async function () {
+  it("should check whether any value in a list is non-zero", async () => {
+    this.timeout(0);
+
+    const logicFactory = await ethers.getContractFactory("LogicTest");
+
+    const constants = [0, 1, 2, 3];
+
+    const v0 = op(Opcode.VAL, 0);
+    const v1 = op(Opcode.VAL, 1);
+    const v2 = op(Opcode.VAL, 2);
+    const v3 = op(Opcode.VAL, 3);
+
+    // prettier-ignore
+    const source0 = concat([
+      v1,
+      v2,
+      v3,
+      op(Opcode.ANY, 3),
+    ]);
+
+    const logic0 = (await logicFactory.deploy({
+      sources: [source0],
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as LogicTest & Contract;
+
+    const result0 = await logic0.run();
+
+    assert(result0.eq(1), `returned wrong value from any, got ${result0}`);
+
+    // prettier-ignore
+    const source1 = concat([
+      v0,
+      v0,
+      op(Opcode.ANY, 2),
+    ]);
+
+    const logic1 = (await logicFactory.deploy({
+      sources: [source1],
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as LogicTest & Contract;
+
+    const result1 = await logic1.run();
+
+    assert(result1.isZero(), `returned wrong value from any, got ${result1}`);
+
+    // prettier-ignore
+    const source2 = concat([
+      v0,
+      v0,
+      v3,
+      op(Opcode.ANY, 3),
+    ]);
+
+    const logic2 = (await logicFactory.deploy({
+      sources: [source2],
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as LogicTest & Contract;
+
+    const result2 = await logic2.run();
+
+    assert(result2.eq(3), `returned wrong value from any, got ${result2}`);
+  });
+
+  it("should check whether every value in a list is non-zero", async () => {
+    this.timeout(0);
+
+    const logicFactory = await ethers.getContractFactory("LogicTest");
+
+    const constants = [0, 1, 2, 3];
+
+    const v0 = op(Opcode.VAL, 0);
+    const v1 = op(Opcode.VAL, 1);
+    const v2 = op(Opcode.VAL, 2);
+    const v3 = op(Opcode.VAL, 3);
+
+    // prettier-ignore
+    const source0 = concat([
+      v1,
+      v2,
+      v3,
+      op(Opcode.EVERY, 3),
+    ]);
+
+    const logic0 = (await logicFactory.deploy({
+      sources: [source0],
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as LogicTest & Contract;
+
+    const result0 = await logic0.run();
+
+    assert(result0.eq(1), `returned wrong value from every, got ${result0}`);
+
+    // prettier-ignore
+    const source1 = concat([
+      v0,
+      v1,
+      v2,
+      op(Opcode.EVERY, 3),
+    ]);
+
+    const logic1 = (await logicFactory.deploy({
+      sources: [source1],
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as LogicTest & Contract;
+
+    const result1 = await logic1.run();
+
+    assert(result1.isZero(), `returned wrong value from every, got ${result1}`);
+
+    // prettier-ignore
+    const source2 = concat([
+      v0,
+      v3,
+      op(Opcode.EVERY, 2),
+    ]);
+
+    const logic2 = (await logicFactory.deploy({
+      sources: [source2],
+      constants,
+      argumentsLength: 0,
+      stackLength: 3,
+    })) as LogicTest & Contract;
+
+    const result2 = await logic2.run();
+
+    assert(result2.isZero(), `returned wrong value from every, got ${result2}`);
+  });
+
   it("should perform ternary 'eager if' operation on 3 values on the stack", async () => {
     this.timeout(0);
 
