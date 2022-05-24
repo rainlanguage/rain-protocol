af9a3f638d8084d96cbf6898c035920544647879
marcusnewton <marcus.newton@hotmail.com>
2022-05-24 15:09:51 +1000

    resolve indexed event data

--- utils/events/index.ts
+++ utils/events/index.ts
@@ -30,7 +30,7 @@ export const getEvents = async (
   }
 
   return eventObjs.map((eventObj) =>
-    contract.interface.decodeEventLog(eventName, eventObj.data)
+    contract.interface.decodeEventLog(eventName, eventObj.data, eventObj.topics)
   );
 };
 
@@ -48,5 +48,23 @@ export const getEventArgs = async (
   contract: Contract,
   contractAddressOverride: string = null
 ): Promise<Result> => {
-  return (await getEvents(tx, eventName, contract, contractAddressOverride))[0];
+  const address = contractAddressOverride
+    ? contractAddressOverride
+    : contract.address;
+
+  const eventObj = (await tx.wait()).events.find(
+    (x) =>
+      x.topics[0] == contract.filters[eventName]().topics[0] &&
+      x.address == address
+  );
+
+  if (!eventObj) {
+    throw new Error(`Could not find event ${eventName} at address ${address}`);
+  }
+
+  return contract.interface.decodeEventLog(
+    eventName,
+    eventObj.data,
+    eventObj.topics
+  );
 };
