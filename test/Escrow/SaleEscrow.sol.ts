a310f9df1b962e500a74885560fa290e556dd819
SocioDroid <sociodroid2019@gmail.com>
2022-06-08 01:51:26 +0530

    Escrow tests splitting

--- test/Escrow/SaleEscrow.sol.ts
+++ test/Escrow/SaleEscrow.sol.ts
@@ -82,109 +82,69 @@ describe("SaleEscrow", async function () {
     await saleFactory.deployed();
   });
 
-  it("if a sale creates a redeemable token that doesn't freeze, it should not be possible to drain the RedeemableERC20ClaimEscrow by repeatedly claiming after moving the same funds somewhere else (in the case of failed Sale)", async function () {
+  it("should return reserve and token addresses, and escrow status of Pending, after Sale initialisation", async function () {
     this.timeout(0);
 
     const signers = await ethers.getSigners();
+    const deployer = signers[0];
 
-    const signer1 = signers[1];
-    const signer2 = signers[2];
-
-    // Deploy global Claim contract
-    const rTKNClaimEscrowFactory = await ethers.getContractFactory(
-      "RedeemableERC20ClaimEscrow"
-    );
-    const rTKNClaimEscrow =
-      (await rTKNClaimEscrowFactory.deploy()) as RedeemableERC20ClaimEscrow &
-        Contract;
-
-    const tokenFactory = await ethers.getContractFactory("ReserveToken");
-    const reserve = (await tokenFactory.deploy()) as Contract & IERC20;
-    const rTKN = (await tokenFactory.deploy()) as Contract & IERC20;
-
-    await reserve.deployed();
-    await rTKN.deployed();
+    const totalTokenSupply = ethers.BigNumber.from("2000").mul(Util.ONE);
+    const redeemableERC20Config = {
+      name: "Token",
+      symbol: "TKN",
+      distributor: deployer.address,
+      initialSupply: totalTokenSupply,
+    };
+    const redeemableERC20 = (await Util.redeemableERC20Deploy(deployer, {
+      reserve: reserve.address,
+      erc20Config: redeemableERC20Config,
+      tier: readWriteTier.address,
+      minimumTier: 0,
+      distributionEndForwardingAddress: Util.zeroAddress,
+    })) as RedeemableERC20 & Contract;
 
     const saleFactory = await ethers.getContractFactory("MockISale");
-    const sale1 = (await saleFactory.deploy()) as Contract & MockISale;
-    const sale2 = (await saleFactory.deploy()) as Contract & MockISale;
+    const sale = (await saleFactory.deploy()) as Contract & MockISale;
 
-    // Two identical successful sales with some tokens to distribute.
-    const sales: Array<Contract & MockISale> = [sale1, sale2];
-    for (const sale of sales) {
-      await sale.deployed();
-      await sale.setReserve(reserve.address);
-      await sale.setToken(rTKN.address);
-      await sale.setSaleStatus(SaleStatus.Success);
-      await reserve.approve(rTKNClaimEscrow.address, 1000);
-      await rTKNClaimEscrow.deposit(sale.address, reserve.address, 1000);
-    }
+    await sale.setReserve(reserve.address);
+    await sale.setToken(redeemableERC20.address);
+    await sale.setSaleStatus(Status.PENDING);
 
-    assert(
-      (await rTKN.balanceOf(signer1.address)).eq(0),
-      "signer 1 had token balance prematurely"
-    );
-    // If signer1 has all the token they should get all deposited reserve.
-    await rTKN.transfer(signer1.address, await rTKN.totalSupply());
+    const saleReserve = await sale.reserve();
+    const saleToken = await sale.token();
+    const saleStatus: SaleStatus = await sale.saleStatus();
 
-    await rTKNClaimEscrow
-      .connect(signer1)
-      .withdraw(sale1.address, reserve.address, await rTKN.totalSupply());
+    // sanity check
+    assert(saleStatus === SaleStatus.Pending, "wrong sale status: not Pending");
 
-    assert(
-      (await reserve.balanceOf(signer1.address)).eq(1000),
-      `signer 1 did not withdraw the deposited reserve`
-    );
+    const saleEscrowWrapper = (await Util.basicDeploy(
+      "SaleEscrowWrapper",
+      {}
+    )) as SaleEscrowWrapper & Contract;
 
-    // At this point signer 1 has withdrawn all they can for sale1.
-    await Util.assertError(
-      async () =>
-        await rTKNClaimEscrow
-          .connect(signer1)
-          .withdraw(sale1.address, reserve.address, await rTKN.totalSupply()),
-      "ZERO_WITHDRAW",
-      "didn't prevent signer 1 from withdrawing a second time"
-    );
+    await saleEscrowWrapper.fetchReserve(sale.address);
+    await saleEscrowWrapper.fetchToken(sale.address);
+    await saleEscrowWrapper.fetchEscrowStatus(sale.address);
 
-    // At this point there is still 1000 reserve in the escrow (for sale2).
-    // We want to prevent signer 1 from colluding with signer 2 to withdraw
-    // more funds from sale1 than were ever deposited for it. If this were
-    // possible then malicious ISale contracts can steal from honest contracts.
-    await rTKN
-      .connect(signer1)
-      .transfer(signer2.address, await rTKN.totalSupply());
-    // This has to underflow here as signer2 is now trying to withdraw 1000
-    // reserve tokens, which means 2000 reserve tokens total withdrawn from
-    // sale1 vs. 1000 tokens deposited for sale1.
-    await Util.assertError(
-      async () =>
-        await rTKNClaimEscrow
-          .connect(signer2)
-          .withdraw(sale1.address, reserve.address, await rTKN.totalSupply()),
-      "underflowed",
-      "didn't prevent signer 2 from withdrawing from sale1 what was already withdrawn"
-    );
-
-    // However, it's entirely possible for signer2 to withdraw 1000 tokens
-    // from sale2 as sale1 and sale2 share the same non-reserve token.
-    await rTKNClaimEscrow
-      .connect(signer2)
-      .withdraw(sale2.address, reserve.address, await rTKN.totalSupply());
+    const saleEscrowReserve = await saleEscrowWrapper.getReserve(sale.address);
+    const saleEscrowToken = await saleEscrowWrapper.getToken(sale.address);
+    const saleEscrowStatus: EscrowStatus =
+      await saleEscrowWrapper.getEscrowStatus(sale.address);
 
     assert(
-      (await reserve.balanceOf(signer1.address)).eq(1000),
-      `signer 1 has incorrect reserve.`
+      saleReserve === saleEscrowReserve,
+      "sale escrow fetched incorrect reserve address"
     );
     assert(
-      (await reserve.balanceOf(signer2.address)).eq(1000),
-      `signer 2 has incorrect reserve.`
+      saleToken === saleEscrowToken,
+      "sale escrow fetched incorrect token address"
     );
     assert(
-      (await reserve.balanceOf(rTKNClaimEscrow.address)).eq(0),
-      `escrow has incorrect reserve.`
+      saleEscrowStatus === EscrowStatus.Pending,
+      "wrong sale escrow status: not Pending"
     );
   });
-
+  
   it("should prevent 'malicious' sale contract from modifying fail status", async function () {
     this.timeout(0);
 
@@ -348,66 +308,107 @@ describe("SaleEscrow", async function () {
     );
   });
 
-  it("should return reserve and token addresses, and escrow status of Pending, after Sale initialisation", async function () {
+  it("if a sale creates a redeemable token that doesn't freeze, it should not be possible to drain the RedeemableERC20ClaimEscrow by repeatedly claiming after moving the same funds somewhere else (in the case of failed Sale)", async function () {
     this.timeout(0);
 
     const signers = await ethers.getSigners();
-    const deployer = signers[0];
 
-    const totalTokenSupply = ethers.BigNumber.from("2000").mul(Util.ONE);
-    const redeemableERC20Config = {
-      name: "Token",
-      symbol: "TKN",
-      distributor: deployer.address,
-      initialSupply: totalTokenSupply,
-    };
-    const redeemableERC20 = (await Util.redeemableERC20Deploy(deployer, {
-      reserve: reserve.address,
-      erc20Config: redeemableERC20Config,
-      tier: readWriteTier.address,
-      minimumTier: 0,
-      distributionEndForwardingAddress: Util.zeroAddress,
-    })) as RedeemableERC20 & Contract;
+    const signer1 = signers[1];
+    const signer2 = signers[2];
+
+    // Deploy global Claim contract
+    const rTKNClaimEscrowFactory = await ethers.getContractFactory(
+      "RedeemableERC20ClaimEscrow"
+    );
+    const rTKNClaimEscrow =
+      (await rTKNClaimEscrowFactory.deploy()) as RedeemableERC20ClaimEscrow &
+        Contract;
+
+    const tokenFactory = await ethers.getContractFactory("ReserveToken");
+    const reserve = (await tokenFactory.deploy()) as Contract & IERC20;
+    const rTKN = (await tokenFactory.deploy()) as Contract & IERC20;
+
+    await reserve.deployed();
+    await rTKN.deployed();
 
     const saleFactory = await ethers.getContractFactory("MockISale");
-    const sale = (await saleFactory.deploy()) as Contract & MockISale;
+    const sale1 = (await saleFactory.deploy()) as Contract & MockISale;
+    const sale2 = (await saleFactory.deploy()) as Contract & MockISale;
 
-    await sale.setReserve(reserve.address);
-    await sale.setToken(redeemableERC20.address);
-    await sale.setSaleStatus(Status.PENDING);
-
-    const saleReserve = await sale.reserve();
-    const saleToken = await sale.token();
-    const saleStatus: SaleStatus = await sale.saleStatus();
-
-    // sanity check
-    assert(saleStatus === SaleStatus.Pending, "wrong sale status: not Pending");
-
-    const saleEscrowWrapper = (await Util.basicDeploy(
-      "SaleEscrowWrapper",
-      {}
-    )) as SaleEscrowWrapper & Contract;
-
-    await saleEscrowWrapper.fetchReserve(sale.address);
-    await saleEscrowWrapper.fetchToken(sale.address);
-    await saleEscrowWrapper.fetchEscrowStatus(sale.address);
-
-    const saleEscrowReserve = await saleEscrowWrapper.getReserve(sale.address);
-    const saleEscrowToken = await saleEscrowWrapper.getToken(sale.address);
-    const saleEscrowStatus: EscrowStatus =
-      await saleEscrowWrapper.getEscrowStatus(sale.address);
+    // Two identical successful sales with some tokens to distribute.
+    const sales: Array<Contract & MockISale> = [sale1, sale2];
+    for (const sale of sales) {
+      await sale.deployed();
+      await sale.setReserve(reserve.address);
+      await sale.setToken(rTKN.address);
+      await sale.setSaleStatus(SaleStatus.Success);
+      await reserve.approve(rTKNClaimEscrow.address, 1000);
+      await rTKNClaimEscrow.deposit(sale.address, reserve.address, 1000);
+    }
 
     assert(
-      saleReserve === saleEscrowReserve,
-      "sale escrow fetched incorrect reserve address"
+      (await rTKN.balanceOf(signer1.address)).eq(0),
+      "signer 1 had token balance prematurely"
+    );
+    // If signer1 has all the token they should get all deposited reserve.
+    await rTKN.transfer(signer1.address, await rTKN.totalSupply());
+
+    await rTKNClaimEscrow
+      .connect(signer1)
+      .withdraw(sale1.address, reserve.address, await rTKN.totalSupply());
+
+    assert(
+      (await reserve.balanceOf(signer1.address)).eq(1000),
+      `signer 1 did not withdraw the deposited reserve`
+    );
+
+    // At this point signer 1 has withdrawn all they can for sale1.
+    await Util.assertError(
+      async () =>
+        await rTKNClaimEscrow
+          .connect(signer1)
+          .withdraw(sale1.address, reserve.address, await rTKN.totalSupply()),
+      "ZERO_WITHDRAW",
+      "didn't prevent signer 1 from withdrawing a second time"
+    );
+
+    // At this point there is still 1000 reserve in the escrow (for sale2).
+    // We want to prevent signer 1 from colluding with signer 2 to withdraw
+    // more funds from sale1 than were ever deposited for it. If this were
+    // possible then malicious ISale contracts can steal from honest contracts.
+    await rTKN
+      .connect(signer1)
+      .transfer(signer2.address, await rTKN.totalSupply());
+    // This has to underflow here as signer2 is now trying to withdraw 1000
+    // reserve tokens, which means 2000 reserve tokens total withdrawn from
+    // sale1 vs. 1000 tokens deposited for sale1.
+    await Util.assertError(
+      async () =>
+        await rTKNClaimEscrow
+          .connect(signer2)
+          .withdraw(sale1.address, reserve.address, await rTKN.totalSupply()),
+      "underflowed",
+      "didn't prevent signer 2 from withdrawing from sale1 what was already withdrawn"
+    );
+
+    // However, it's entirely possible for signer2 to withdraw 1000 tokens
+    // from sale2 as sale1 and sale2 share the same non-reserve token.
+    await rTKNClaimEscrow
+      .connect(signer2)
+      .withdraw(sale2.address, reserve.address, await rTKN.totalSupply());
+
+    assert(
+      (await reserve.balanceOf(signer1.address)).eq(1000),
+      `signer 1 has incorrect reserve.`
     );
     assert(
-      saleToken === saleEscrowToken,
-      "sale escrow fetched incorrect token address"
+      (await reserve.balanceOf(signer2.address)).eq(1000),
+      `signer 2 has incorrect reserve.`
     );
     assert(
-      saleEscrowStatus === EscrowStatus.Pending,
-      "wrong sale escrow status: not Pending"
+      (await reserve.balanceOf(rTKNClaimEscrow.address)).eq(0),
+      `escrow has incorrect reserve.`
     );
   });
+
 });
