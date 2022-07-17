0f3597f7869e6cba1c261d93667b8bc125dd74d8
thedavidmeister <thedavidmeister@gmail.com>
2022-07-17 19:52:57 +0400

    wip on simplified opcode

--- contracts/vm/LibStackTop.sol
+++ contracts/vm/LibStackTop.sol
@@ -4,11 +4,24 @@ pragma solidity ^0.8.15;
 type StackTop is uint;
 
 library LibStackTop {
-    function push(StackTop stackTop_, uint value_) internal pure returns (StackTop) {
+    function push(StackTop stackTop_, uint a_) internal pure returns (StackTop) {
         assembly ("memory-safe") {
-            mstore(stackTop_, value_)
+            mstore(stackTop_, a_)
             stackTop_ := add(stackTop_, 0x20)
         }
         return stackTop_;
+    }
+
+    function peek(StackTop stackTop_) internal pure returns (StackTop peek_, uint a_) {
+        assembly ("memory-safe") {
+            peek_ := sub(stackTop_, 0x20)
+            a_ := mload(peek_)
+        }
+    }
+
+    function set(StackTop stackTop_, a_) internal pure returns (StackTop) {
+        assembly ("memory-safe") {
+            mstore(stackTop_, a_)
+        }
     }
 }
--- contracts/vm/ops/erc20/OpERC20TotalSupply.sol
+++ contracts/vm/ops/erc20/OpERC20TotalSupply.sol
@@ -7,22 +7,28 @@ import "../../LibStackTop.sol";
 /// @title OpERC20TotalSupply
 /// @notice Opcode for ERC20 `totalSupply`.
 library OpERC20TotalSupply {
+    using LibStackTop for StackTop;
+
     // Stack the return of `totalSupply`.
-    function totalSupply(uint256, StackTop stackTopLocation_)
+    function totalSupply(uint256, StackTop stackTop_)
         internal
         view
         returns (StackTop)
     {
-        uint256 location_;
-        uint256 token_;
-        assembly ("memory-safe") {
-            location_ := sub(stackTopLocation_, 0x20)
-            token_ := mload(location_)
-        }
-        uint256 supply_ = IERC20(address(uint160(token_))).totalSupply();
-        assembly ("memory-safe") {
-            mstore(location_, supply_)
-        }
-        return stackTopLocation_;
+        // uint256 location_;
+        // uint256 token_;
+        // assembly ("memory-safe") {
+        //     location_ := sub(stackTop_, 0x20)
+        //     token_ := mload(location_)
+        // }
+        (StackTop peek_, uint token_) = stackTop_.peek();
+        peek_.set(
+            IERC20(address(uint160(token_))).totalSupply()
+        );
+        // return stackTop_;
+        // assembly ("memory-safe") {
+        //     mstore(location_, supply_)
+        // }
+        return stackTop_;
     }
 }
