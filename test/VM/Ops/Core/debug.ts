d3c3cc4361e85dca1ec13229ad15d611650ea276
NanezX <victor.nanezj@gmail.com>
2022-08-18 22:56:00 -0400

    extented debug op test using loop and do while mechanics

--- test/VM/Ops/Core/debug.ts
+++ test/VM/Ops/Core/debug.ts
@@ -10,6 +10,7 @@ import {
   memoryOperand,
   MemoryType,
   callOperand,
+  loopNOperand,
 } from "../../../../utils/rainvm/vm";
 
 const Opcode = AllStandardOps;
@@ -91,5 +92,73 @@ describe("RainVM debug op", async function () {
     });
 
     await logic.run();
+  });
+
+  it("should be able to log when is used within a source from DO_WHILE op", async () => {
+    const constants = [3, 2, 7];
+
+    // prettier-ignore
+    const sourceMAIN = concat([
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0)),
+          op(Opcode.STATE, memoryOperand(MemoryType.Stack, 0)),
+          op(Opcode.STATE, memoryOperand(MemoryType.Constant, 2)),
+        op(Opcode.LESS_THAN),
+      op(Opcode.DO_WHILE, 1), // Source to run is on index 1
+    ]);
+
+    // prettier-ignore
+    const sourceWHILE = concat([
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1)),
+      op(Opcode.ADD, 2),
+        op(Opcode.STATE, memoryOperand(MemoryType.Stack, 0)),
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 2)),
+      op(Opcode.LESS_THAN),
+      op(Opcode.DEBUG, Debug.Stack),
+    ]);
+
+    await logic.initialize({
+      sources: [sourceMAIN, sourceWHILE],
+      constants,
+    });
+
+    await logic.run();
+  });
+
+  it("should be able to log when is used within a source from LOOP_N op", async () => {
+    const n = 5;
+    const initialValue = 2;
+    const incrementValue = 1;
+
+    const constants = [initialValue, incrementValue];
+
+    // prettier-ignore
+    const sourceADD = concat([
+          op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1)),
+        op(Opcode.ADD, 2),
+        op(Opcode.DEBUG, Debug.Stack),
+      ]);
+
+    // prettier-ignore
+    const sourceMAIN = concat([
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0)),
+      op(Opcode.LOOP_N, loopNOperand(n, 1))
+    ]);
+
+    await logic.initialize({
+      sources: [sourceMAIN, sourceADD],
+      constants,
+    });
+
+    let expectedResult = initialValue;
+    for (let i = 0; i < n; i++) {
+      expectedResult += incrementValue;
+    }
+
+    await logic.run();
+    const result0 = await logic.stackTop();
+    assert(
+      result0.eq(expectedResult),
+      `Invalid output, expected ${expectedResult}, actual ${result0}`
+    );
   });
 });
