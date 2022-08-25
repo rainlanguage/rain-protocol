cc5472baa9e063587bdb1689a68469f174c7f83f
thedavidmeister <thedavidmeister@gmail.com>
2022-08-25 19:41:21 +0400

    remove irrelevant test

--- test/VM/VMStateBuilder/ptrSource.ts
+++ test/VM/VMStateBuilder/ptrSource.ts
@@ -24,16 +24,4 @@ describe("RainVMIntegrity ptrSource", async function () {
       integrity.address
     )) as AllStandardOpsTest;
   });
-
-  it("should error when script length is odd", async () => {
-    const constants = [];
-
-    const sources = [concat([bytify(Opcode.BLOCK_NUMBER)])];
-
-    await assertError(
-      async () => await logic.initialize({ sources, constants }),
-      "ODD_SOURCE_LENGTH",
-      "did not error when script length is odd"
-    );
-  });
 });
