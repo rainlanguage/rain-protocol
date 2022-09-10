5db6adfac4bef12ea1f7400640ba7dd6944fe979
NanezX <victor.nanezj@gmail.com>
2022-09-10 01:22:27 -0400

    Dance libraries

---
+++ echidna/Dance/LibCommitmentEchidna.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: CAL
+pragma solidity 0.8.15;
+
+import {LibCommitment, Commitment, Secret} from "../../contracts/dance/SeedDance.sol";
+
+/// @title LibCommitmentEchidna
+/// Wrapper around the `LibCommitment` library for echidna fuzz testing.
+contract LibCommitmentEchidna {
+    // Fuzz testing against `eq()` function
+    function Eq(uint256 a_, uint256 b_) external pure {
+        bool result = LibCommitment.eq(
+            Commitment.wrap(a_),
+            Commitment.wrap(b_)
+        );
+
+        bool resultExpected = a_ == b_;
+
+        assert(result == resultExpected);
+    }
+
+    // Fuzz testing against `fromSecret()` function
+    function FromSecret(uint256 a_) external pure {
+        Secret input = Secret.wrap(a_);
+
+        Commitment result = LibCommitment.fromSecret(input);
+
+        uint256 resultExpected = uint256(keccak256(abi.encodePacked(a_)));
+
+        assert(Commitment.unwrap(result) == resultExpected);
+    }
+
+    // Fuzz testing against `nil()` function
+    function Nil() external pure {
+        Commitment result = LibCommitment.nil();
+
+        assert(Commitment.unwrap(result) == 0);
+    }
+}
---
+++ echidna/Dance/LibSeedEchidna.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: CAL
+pragma solidity 0.8.15;
+
+import {LibSeed, Seed} from "../../contracts/dance/SeedDance.sol";
+
+/// @title LibSeedEchidna
+/// Wrapper around the `LibSeed` library for echidna fuzz testing.
+contract LibSeedEchidna {
+    // Test the `LibSeed.with()` function evaluating the outputs from differents `val_` inputs.
+    function WithDiffInputs(
+        Seed seedOrigin,
+        uint256 val1_,
+        uint256 val2_
+    ) external pure {
+        require(val1_ != val2_, "SAME_INPUTS");
+
+        Seed newSeed1 = LibSeed.with(seedOrigin, val1_);
+        Seed newSeed2 = LibSeed.with(seedOrigin, val2_);
+
+        // Origin seed should not generate an equal output seed
+        assert(Seed.unwrap(seedOrigin) != Seed.unwrap(newSeed1));
+        assert(Seed.unwrap(seedOrigin) != Seed.unwrap(newSeed2));
+
+        // Same seed and but different val should NOT generate the same output
+        assert(Seed.unwrap(newSeed1) != Seed.unwrap(newSeed2));
+    }
+
+    // Test the `LibSeed.with()` function checking the deterministic outputs from same seed and val.
+    function WithDeterministic(Seed seedOrigin, uint256 val_) external pure {
+        Seed newSeed1 = LibSeed.with(seedOrigin, val_);
+
+        //  Output seed should not be equal to origin seed
+        assert(Seed.unwrap(newSeed1) != Seed.unwrap(seedOrigin));
+
+        Seed newSeed2 = LibSeed.with(seedOrigin, val_);
+
+        // Same seed and val should generate the same output everytime
+        assert(Seed.unwrap(newSeed1) == Seed.unwrap(newSeed2));
+    }
+}
