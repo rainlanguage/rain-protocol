c967e0d3d64eca79777f2040ce6482b1487cb402
thedavidmeister <thedavidmeister@gmail.com>
2022-09-15 19:40:06 +0400

    disable optimiser

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,7 +20,7 @@ const config: HardhatUserConfig = {
         version: "0.8.17",
         settings: {
           optimizer: {
-            enabled: true,
+            enabled: false,
             runs: 1000000000,
             details: {
               peephole: true,
