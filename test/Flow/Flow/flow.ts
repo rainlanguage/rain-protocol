b7126f2b5dc3496f448e227482776a344ed2dbf3
marcusnewton <marcus.newton@hey.com>
2022-09-15 15:47:14 +1000

    fix tests

--- test/Flow/Flow/flow.ts
+++ test/Flow/Flow/flow.ts
@@ -40,6 +40,352 @@ describe("Flow flow tests", async function () {
     await flowFactory.deployed();
   });
 
+  it("should preview defined flow IO for ERC1155 (multi element arrays)", async () => {
+    const signers = await ethers.getSigners();
+    const deployer = signers[0];
+
+    const erc1155A = (await basicDeploy(
+      "ReserveTokenERC1155",
+      {}
+    )) as ReserveTokenERC1155;
+    await erc1155A.initialize();
+    const erc1155B = (await basicDeploy(
+      "ReserveTokenERC1155",
+      {}
+    )) as ReserveTokenERC1155;
+    await erc1155B.initialize();
+
+    const flowIO: FlowIOStruct = {
+      inputNative: 0,
+      outputNative: 0,
+      inputs20: [],
+      outputs20: [],
+      inputs721: [],
+      outputs721: [],
+      inputs1155: [
+        { token: erc1155A.address, id: 1, amount: 2 },
+        { token: erc1155B.address, id: 3, amount: 4 },
+      ],
+      outputs1155: [
+        { token: erc1155A.address, id: 5, amount: 6 },
+        { token: erc1155B.address, id: 7, amount: 8 },
+      ],
+    };
+
+    const constants = [
+      RAIN_FLOW_SENTINEL,
+      1,
+      flowIO.inputNative,
+      flowIO.outputNative,
+      flowIO.inputs1155[0].token,
+      flowIO.inputs1155[0].id,
+      flowIO.inputs1155[0].amount,
+      flowIO.inputs1155[1].token,
+      flowIO.inputs1155[1].id,
+      flowIO.inputs1155[1].amount,
+      flowIO.outputs1155[0].token,
+      flowIO.outputs1155[0].id,
+      flowIO.outputs1155[0].amount,
+      flowIO.outputs1155[1].token,
+      flowIO.outputs1155[1].id,
+      flowIO.outputs1155[1].amount,
+    ];
+
+    const SENTINEL = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0));
+    const CAN_FLOW = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1));
+    const FLOWIO_INPUT_NATIVE = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 2));
+    const FLOWIO_OUTPUT_NATIVE = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 3));
+
+    const FLOWIO_INPUT_ERC1155_TOKEN_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 4));
+    const FLOWIO_INPUT_ERC1155_ID_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 5));
+    const FLOWIO_INPUT_ERC1155_AMOUNT_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 6));
+    const FLOWIO_INPUT_ERC1155_TOKEN_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 7));
+    const FLOWIO_INPUT_ERC1155_ID_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 8));
+    const FLOWIO_INPUT_ERC1155_AMOUNT_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 9));
+
+    const FLOWIO_OUTPUT_ERC1155_TOKEN_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 10));
+    const FLOWIO_OUTPUT_ERC1155_ID_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 11));
+    const FLOWIO_OUTPUT_ERC1155_AMOUNT_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 12));
+    const FLOWIO_OUTPUT_ERC1155_TOKEN_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 13));
+    const FLOWIO_OUTPUT_ERC1155_ID_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 14));
+    const FLOWIO_OUTPUT_ERC1155_AMOUNT_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 15));
+
+    // prettier-ignore
+    const sourceCanFlow = concat([
+      CAN_FLOW(), // true
+    ]);
+
+    const sourceFlowIO = concat([
+      SENTINEL(),
+      FLOWIO_OUTPUT_ERC1155_TOKEN_A(),
+      FLOWIO_OUTPUT_ERC1155_ID_A(),
+      FLOWIO_OUTPUT_ERC1155_AMOUNT_A(),
+      FLOWIO_OUTPUT_ERC1155_TOKEN_B(),
+      FLOWIO_OUTPUT_ERC1155_ID_B(),
+      FLOWIO_OUTPUT_ERC1155_AMOUNT_B(),
+      SENTINEL(),
+      FLOWIO_INPUT_ERC1155_TOKEN_A(),
+      FLOWIO_INPUT_ERC1155_ID_A(),
+      FLOWIO_INPUT_ERC1155_AMOUNT_A(),
+      FLOWIO_INPUT_ERC1155_TOKEN_B(),
+      FLOWIO_INPUT_ERC1155_ID_B(),
+      FLOWIO_INPUT_ERC1155_AMOUNT_B(),
+      SENTINEL(),
+      SENTINEL(),
+      SENTINEL(),
+      SENTINEL(),
+      FLOWIO_OUTPUT_NATIVE(),
+      FLOWIO_INPUT_NATIVE(),
+    ]);
+
+    const sources = [sourceCanFlow, sourceFlowIO];
+
+    const stateConfigStruct: StateConfigStruct = {
+      sources,
+      constants,
+    };
+
+    const flow = await flowDeploy(deployer, flowFactory, stateConfigStruct);
+
+    const flowIOPreview = await flow.previewFlow(1, 1234);
+
+    compareStructs(flowIOPreview, flowIO, true);
+  });
+
+  it("should preview defined flow IO for ERC721 (multi element arrays)", async () => {
+    const signers = await ethers.getSigners();
+    const deployer = signers[0];
+
+    const erc721A = (await basicDeploy(
+      "ReserveTokenERC721",
+      {}
+    )) as ReserveTokenERC721;
+    await erc721A.initialize();
+    const erc721B = (await basicDeploy(
+      "ReserveTokenERC721",
+      {}
+    )) as ReserveTokenERC721;
+    await erc721B.initialize();
+
+    const flowIO: FlowIOStruct = {
+      inputNative: 0,
+      outputNative: 0,
+      inputs20: [],
+      outputs20: [],
+      inputs721: [
+        { token: erc721A.address, id: 1 },
+        { token: erc721B.address, id: 2 },
+      ],
+      outputs721: [
+        { token: erc721A.address, id: 3 },
+        { token: erc721B.address, id: 4 },
+      ],
+      inputs1155: [],
+      outputs1155: [],
+    };
+
+    const constants = [
+      RAIN_FLOW_SENTINEL,
+      1,
+      flowIO.inputNative,
+      flowIO.outputNative,
+      flowIO.inputs721[0].token,
+      flowIO.inputs721[0].id,
+      flowIO.inputs721[1].token,
+      flowIO.inputs721[1].id,
+      flowIO.outputs721[0].token,
+      flowIO.outputs721[0].id,
+      flowIO.outputs721[1].token,
+      flowIO.outputs721[1].id,
+    ];
+
+    const SENTINEL = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0));
+    const CAN_FLOW = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1));
+    const FLOWIO_INPUT_NATIVE = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 2));
+    const FLOWIO_OUTPUT_NATIVE = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 3));
+
+    const FLOWIO_INPUT_ERC721_TOKEN_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 4));
+    const FLOWIO_INPUT_ERC721_ID_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 5));
+    const FLOWIO_INPUT_ERC721_TOKEN_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 6));
+    const FLOWIO_INPUT_ERC721_ID_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 7));
+
+    const FLOWIO_OUTPUT_ERC721_TOKEN_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 8));
+    const FLOWIO_OUTPUT_ERC721_ID_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 9));
+    const FLOWIO_OUTPUT_ERC721_TOKEN_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 10));
+    const FLOWIO_OUTPUT_ERC721_ID_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 11));
+
+    // prettier-ignore
+    const sourceCanFlow = concat([
+      CAN_FLOW(), // true
+    ]);
+
+    const sourceFlowIO = concat([
+      SENTINEL(),
+      SENTINEL(),
+      SENTINEL(),
+      FLOWIO_OUTPUT_ERC721_TOKEN_A(),
+      FLOWIO_OUTPUT_ERC721_ID_A(),
+      FLOWIO_OUTPUT_ERC721_TOKEN_B(),
+      FLOWIO_OUTPUT_ERC721_ID_B(),
+      SENTINEL(),
+      FLOWIO_INPUT_ERC721_TOKEN_A(),
+      FLOWIO_INPUT_ERC721_ID_A(),
+      FLOWIO_INPUT_ERC721_TOKEN_B(),
+      FLOWIO_INPUT_ERC721_ID_B(),
+      SENTINEL(),
+      SENTINEL(),
+      FLOWIO_OUTPUT_NATIVE(),
+      FLOWIO_INPUT_NATIVE(),
+    ]);
+
+    const sources = [sourceCanFlow, sourceFlowIO];
+
+    const stateConfigStruct: StateConfigStruct = {
+      sources,
+      constants,
+    };
+
+    const flow = await flowDeploy(deployer, flowFactory, stateConfigStruct);
+
+    const flowIOPreview = await flow.previewFlow(1, 1234);
+
+    compareStructs(flowIOPreview, flowIO, true);
+  });
+
+  it("should preview defined flow IO for ERC20 (multi element arrays)", async () => {
+    const signers = await ethers.getSigners();
+    const deployer = signers[0];
+
+    const erc20A = (await basicDeploy("ReserveToken", {})) as ReserveToken;
+    await erc20A.initialize();
+    const erc20B = (await basicDeploy("ReserveToken", {})) as ReserveToken;
+    await erc20B.initialize();
+
+    const flowIO: FlowIOStruct = {
+      inputNative: 10,
+      outputNative: 50,
+      inputs20: [
+        { token: erc20A.address, amount: 1 },
+        { token: erc20B.address, amount: 2 },
+      ],
+      outputs20: [
+        { token: erc20A.address, amount: 3 },
+        { token: erc20B.address, amount: 4 },
+      ],
+      inputs721: [],
+      outputs721: [],
+      inputs1155: [],
+      outputs1155: [],
+    };
+
+    const constants = [
+      RAIN_FLOW_SENTINEL,
+      1,
+      flowIO.inputNative,
+      flowIO.outputNative,
+      flowIO.inputs20[0].token,
+      flowIO.inputs20[0].amount,
+      flowIO.inputs20[1].token,
+      flowIO.inputs20[1].amount,
+      flowIO.outputs20[0].token,
+      flowIO.outputs20[0].amount,
+      flowIO.outputs20[1].token,
+      flowIO.outputs20[1].amount,
+    ];
+
+    const SENTINEL = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0));
+    const CAN_FLOW = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1));
+    const FLOWIO_INPUT_NATIVE = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 2));
+    const FLOWIO_OUTPUT_NATIVE = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 3));
+
+    const FLOWIO_INPUT_ERC20_TOKEN_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 4));
+    const FLOWIO_INPUT_ERC20_AMOUNT_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 5));
+    const FLOWIO_INPUT_ERC20_TOKEN_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 6));
+    const FLOWIO_INPUT_ERC20_AMOUNT_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 7));
+
+    const FLOWIO_OUTPUT_ERC20_TOKEN_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 8));
+    const FLOWIO_OUTPUT_ERC20_AMOUNT_A = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 9));
+    const FLOWIO_OUTPUT_ERC20_TOKEN_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 10));
+    const FLOWIO_OUTPUT_ERC20_AMOUNT_B = () =>
+      op(Opcode.STATE, memoryOperand(MemoryType.Constant, 11));
+
+    // prettier-ignore
+    const sourceCanFlow = concat([
+      CAN_FLOW(), // true
+    ]);
+
+    const sourceFlowIO = concat([
+      SENTINEL(),
+      SENTINEL(),
+      SENTINEL(),
+      SENTINEL(),
+      SENTINEL(),
+      FLOWIO_OUTPUT_ERC20_TOKEN_A(),
+      FLOWIO_OUTPUT_ERC20_AMOUNT_A(),
+      FLOWIO_OUTPUT_ERC20_TOKEN_B(),
+      FLOWIO_OUTPUT_ERC20_AMOUNT_B(),
+      SENTINEL(),
+      FLOWIO_INPUT_ERC20_TOKEN_A(),
+      FLOWIO_INPUT_ERC20_AMOUNT_A(),
+      FLOWIO_INPUT_ERC20_TOKEN_B(),
+      FLOWIO_INPUT_ERC20_AMOUNT_B(),
+      FLOWIO_OUTPUT_NATIVE(),
+      FLOWIO_INPUT_NATIVE(),
+    ]);
+
+    const sources = [sourceCanFlow, sourceFlowIO];
+
+    const stateConfigStruct: StateConfigStruct = {
+      sources,
+      constants,
+    };
+
+    const flow = await flowDeploy(deployer, flowFactory, stateConfigStruct);
+
+    const flowIOPreview = await flow.previewFlow(1, 1234);
+
+    compareStructs(flowIOPreview, flowIO, true);
+  });
+
   it("should preview defined flow IO for ERC1155 (single element arrays)", async () => {
     const signers = await ethers.getSigners();
     const deployer = signers[0];
@@ -102,13 +448,13 @@ describe("Flow flow tests", async function () {
 
     const sourceFlowIO = concat([
       SENTINEL(),
-      FLOWIO_OUTPUT_ERC1155_AMOUNT(),
-      FLOWIO_OUTPUT_ERC1155_ID(),
       FLOWIO_OUTPUT_ERC1155_TOKEN(),
+      FLOWIO_OUTPUT_ERC1155_ID(),
+      FLOWIO_OUTPUT_ERC1155_AMOUNT(),
       SENTINEL(),
-      FLOWIO_INPUT_ERC1155_AMOUNT(),
-      FLOWIO_INPUT_ERC1155_ID(),
       FLOWIO_INPUT_ERC1155_TOKEN(),
+      FLOWIO_INPUT_ERC1155_ID(),
+      FLOWIO_INPUT_ERC1155_AMOUNT(),
       SENTINEL(),
       SENTINEL(),
       SENTINEL(),
@@ -128,7 +474,7 @@ describe("Flow flow tests", async function () {
 
     const flowIOPreview = await flow.previewFlow(1, 1234);
 
-    compareStructs(flowIOPreview, flowIO, true);
+    compareStructs(flowIOPreview, flowIO);
   });
 
   it("should preview defined flow IO for ERC721 (single element arrays)", async () => {
@@ -189,11 +535,11 @@ describe("Flow flow tests", async function () {
       SENTINEL(),
       SENTINEL(),
       SENTINEL(),
-      FLOWIO_OUTPUT_ERC721_ID(),
       FLOWIO_OUTPUT_ERC721_TOKEN(),
+      FLOWIO_OUTPUT_ERC721_ID(),
       SENTINEL(),
-      FLOWIO_INPUT_ERC721_ID(),
       FLOWIO_INPUT_ERC721_TOKEN(),
+      FLOWIO_INPUT_ERC721_ID(),
       SENTINEL(),
       SENTINEL(),
       FLOWIO_OUTPUT_NATIVE(),
@@ -211,7 +557,7 @@ describe("Flow flow tests", async function () {
 
     const flowIOPreview = await flow.previewFlow(1, 1234);
 
-    compareStructs(flowIOPreview, flowIO, true);
+    compareStructs(flowIOPreview, flowIO);
   });
 
   it("should preview defined flow IO for ERC20 (single element arrays)", async () => {
@@ -291,7 +637,7 @@ describe("Flow flow tests", async function () {
 
     const flowIOPreview = await flow.previewFlow(1, 1234);
 
-    compareStructs(flowIOPreview, flowIO, true);
+    compareStructs(flowIOPreview, flowIO);
   });
 
   it("should not flow if canFlow eval returns 0", async () => {
