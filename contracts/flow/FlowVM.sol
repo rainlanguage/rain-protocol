388fa92b4737e63d6c7d56360e0ecdc55a0060d4
thedavidmeister <thedavidmeister@gmail.com>
2022-09-23 10:42:05 +0400

    add 721 holder to flow

--- contracts/flow/FlowVM.sol
+++ contracts/flow/FlowVM.sol
@@ -6,12 +6,13 @@ import "./libraries/LibFlow.sol";
 import "./FlowIntegrity.sol";
 import "../idempotent/LibIdempotentFlag.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ERC721HolderUpgradeable as ERC721Holder } from "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
 import {ERC1155HolderUpgradeable as ERC1155Holder} from "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
 
 SourceIndex constant CAN_FLOW_ENDPOINT = SourceIndex.wrap(0);
 SourceIndex constant FLOW_ENDPOINT = SourceIndex.wrap(1);
 
-contract FlowVM is ERC1155Holder, StandardVM {
+contract FlowVM is ERC721Holder, ERC1155Holder, StandardVM {
     using LibIdempotentFlag for IdempotentFlag;
     using LibVMState for VMState;
     using LibStackTop for StackTop;
@@ -27,6 +28,7 @@ contract FlowVM is ERC1155Holder, StandardVM {
         internal
         onlyInitializing
     {
+        __ERC721Holder_init();
         __ERC1155Holder_init();
         for (uint i_ = 0; i_ < flows_.length; i_++) {
             uint id_ = uint(keccak256(abi.encode(flows_[i_])));
