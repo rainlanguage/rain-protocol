aa032c25b80d241359158850d574affc05d37af5
SocioDroid <sociodroid2019@gmail.com>
2022-10-25 01:50:44 +0530

    Fixed Flow deploy script, Context tests

--- utils/deploy/flow/flowERC1155/flowERC1155Factory/deploy.ts
+++ utils/deploy/flow/flowERC1155/flowERC1155Factory/deploy.ts
@@ -1,11 +1,17 @@
 import { ethers } from "hardhat";
 import { FlowERC1155Factory } from "../../../../../typechain/contracts/flow/erc1155/FlowERC1155Factory";
+import { standardIntegrityDeploy } from "../../../interpreter/integrity/standardIntegrity/deploy";
+
 export const flowERC1155FactoryDeploy = async () => {
+  const integrity = await standardIntegrityDeploy();
+
   const flowFactoryFactory = await ethers.getContractFactory(
     "FlowERC1155Factory",
     {}
   );
-  const flowFactory = (await flowFactoryFactory.deploy()) as FlowERC1155Factory;
+  const flowFactory = (await flowFactoryFactory.deploy(
+    integrity.address
+  )) as FlowERC1155Factory;
   await flowFactory.deployed();
   return flowFactory;
 };
