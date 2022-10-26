11658178de4f74ae7051db5a4dce857c96e6b5ba
thedavidmeister <thedavidmeister@gmail.com>
2022-10-26 19:07:47 +0400

    check interpreter bytecode hash

--- contracts/interpreter/shared/RainterpreterExpressionDeployerV1.sol
+++ contracts/interpreter/shared/RainterpreterExpressionDeployerV1.sol
@@ -6,11 +6,15 @@ import "../deploy/StandardIntegrity.sol";
 
 bytes constant OPCODE_FUNCTION_POINTERS = hex"";
 bytes32 constant OPCODE_FUNCTION_POINTERS_HASH = keccak256(OPCODE_FUNCTION_POINTERS);
+bytes32 constant INTERPRETER_BYTECODE_HASH = bytes32(0);
 
 contract RainterpreterExpressionDeployerV1 is StandardIntegrity, IExpressionDeployer {
     using LibInterpreterState for StateConfig;
     event SaveInterpreterState(address sender, StateConfig config);
 
+    /// THIS IS NOT A SECURITY CHECK. IT IS AN INTEGRITY CHECK TO PREVENT HONEST
+    /// MISTAKES. IT CANNOT PREVENT EITHER A MALICIOUS INTERPRETER OR DEPLOYER
+    /// FROM BEING EXECUTED.
     constructor (address interpreter_) {
         bytes memory functionPointers_ = IInterpreter(interpreter_).functionPointers();
         console.logBytes(functionPointers_);
@@ -18,6 +22,13 @@ contract RainterpreterExpressionDeployerV1 is StandardIntegrity, IExpressionDepl
             keccak256(functionPointers_) == OPCODE_FUNCTION_POINTERS_HASH,
             "BAD_POINTERS"
         );
+
+        bytes32 interpreterHash_;
+        assembly ("memory-safe") {
+            interpreterHash_ := extcodehash(interpreter_)
+        }
+        console.logBytes(abi.encodePacked(interpreterHash_));
+        require(interpreterHash_ == INTERPRETER_BYTECODE_HASH, "BAD_INTERPRETER_HASH");
     }
 
     function deployExpression(
