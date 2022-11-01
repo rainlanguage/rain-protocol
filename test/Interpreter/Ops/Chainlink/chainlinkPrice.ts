74e534e331c51cb3911d2f6fc8789df0bb11a406
marcusnewton <marcus.newton@hey.com>
2022-11-01 15:02:26 +1100

    chainlink price op tests

--- test/Interpreter/Ops/Chainlink/chainlinkPrice.ts
+++ test/Interpreter/Ops/Chainlink/chainlinkPrice.ts
@@ -4,17 +4,23 @@ import type {
 } from "../../../../typechain";
 import {
   AllStandardOps,
+  assertError,
+  eighteenZeros,
+  getBlockTimestamp,
   memoryOperand,
   MemoryType,
   op,
+  sixZeros,
+  timewarp,
 } from "../../../../utils";
 import { allStandardOpsDeploy } from "../../../../utils/deploy/test/allStandardOps/deploy";
 import { FakeContract, smock } from "@defi-wonderland/smock";
 import { concat } from "ethers/lib/utils";
+import { assert } from "chai";
 
 const Opcode = AllStandardOps;
 
-describe("CHAINLINK_PRICE Opcode test", async function () {
+describe("CHAINLINK_PRICE Opcode tests", async function () {
   let logic: AllStandardOpsTest;
   let fakeChainlinkOracle: FakeContract<AggregatorV3Interface>;
 
@@ -26,24 +32,104 @@ describe("CHAINLINK_PRICE Opcode test", async function () {
     logic = await allStandardOpsDeploy();
   });
 
-  it("should get price from chainlink oracle", async () => {
-    fakeChainlinkOracle.latestRoundData.returns({
-      roundId: 0,
-      answer: 1,
+  it("should revert if price is stale", async () => {
+    const chainlinkPriceData = {
+      roundId: 1,
+      answer: 123 + eighteenZeros,
       startedAt: 2,
-      updatedAt: 3,
+      updatedAt: 1800, // 1800 sec into the future
       answeredInRound: 4,
-    });
+    };
+
+    fakeChainlinkOracle.latestRoundData.returns(chainlinkPriceData);
     fakeChainlinkOracle.decimals.returns(18);
 
     const feed = fakeChainlinkOracle.address;
-    const staleAfter = 10000;
+    const staleAfter = (await getBlockTimestamp()) + 3600;
 
     const sources = [
       concat([
-        op(Opcode.CHAINLINK_PRICE),
         op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0)),
         op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1)),
+        op(Opcode.CHAINLINK_PRICE),
+      ]),
+    ];
+    const constants = [feed, staleAfter];
+
+    await logic.initialize({
+      sources,
+      constants,
+    });
+
+    await timewarp(1900); // updated 100 sec ago
+
+    await logic.run();
+
+    await timewarp(3600); // updated 3700 sec ago (stale)
+
+    await assertError(
+      async () => await logic.run(),
+      "STALE_PRICE",
+      "did not revert when chainlink price was stale"
+    );
+  });
+
+  it("should revert if price is 0", async () => {
+    const chainlinkPriceData = {
+      roundId: 1,
+      answer: 0 + eighteenZeros,
+      startedAt: 2,
+      updatedAt: 3,
+      answeredInRound: 4,
+    };
+
+    fakeChainlinkOracle.latestRoundData.returns(chainlinkPriceData);
+    fakeChainlinkOracle.decimals.returns(18);
+
+    const feed = fakeChainlinkOracle.address;
+    const staleAfter = (await getBlockTimestamp()) + 10000;
+
+    const sources = [
+      concat([
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0)),
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1)),
+        op(Opcode.CHAINLINK_PRICE),
+      ]),
+    ];
+    const constants = [feed, staleAfter];
+
+    await logic.initialize({
+      sources,
+      constants,
+    });
+
+    await assertError(
+      async () => await logic.run(),
+      "MIN_BASE_PRICE",
+      "did not revert when chainlink price was 0"
+    );
+  });
+
+  it("should correctly scale answer from 6 decimal to 18 decimal FP", async () => {
+    const chainlinkPriceData = {
+      roundId: 1,
+      answer: 123 + sixZeros,
+      startedAt: 2,
+      updatedAt: 3,
+      answeredInRound: 4,
+    };
+
+    fakeChainlinkOracle.latestRoundData.returns(chainlinkPriceData);
+    fakeChainlinkOracle.decimals.returns(6);
+
+    const feed = fakeChainlinkOracle.address;
+    const staleAfter = (await getBlockTimestamp()) + 10000;
+
+    const sources = [
+      concat([
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0)),
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1)),
+        op(Opcode.CHAINLINK_PRICE),
       ]),
     ];
     const constants = [feed, staleAfter];
@@ -56,6 +142,41 @@ describe("CHAINLINK_PRICE Opcode test", async function () {
     await logic.run();
     const price_ = await logic.stackTop();
 
-    console.log({ price_ });
+    assert(price_.eq(123 + eighteenZeros));
+  });
+
+  it("should get price from chainlink oracle", async () => {
+    const chainlinkPriceData = {
+      roundId: 1,
+      answer: 123 + eighteenZeros,
+      startedAt: 2,
+      updatedAt: 3,
+      answeredInRound: 4,
+    };
+
+    fakeChainlinkOracle.latestRoundData.returns(chainlinkPriceData);
+    fakeChainlinkOracle.decimals.returns(18);
+
+    const feed = fakeChainlinkOracle.address;
+    const staleAfter = (await getBlockTimestamp()) + 10000;
+
+    const sources = [
+      concat([
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 0)),
+        op(Opcode.STATE, memoryOperand(MemoryType.Constant, 1)),
+        op(Opcode.CHAINLINK_PRICE),
+      ]),
+    ];
+    const constants = [feed, staleAfter];
+
+    await logic.initialize({
+      sources,
+      constants,
+    });
+
+    await logic.run();
+    const price_ = await logic.stackTop();
+
+    assert(price_.eq(123 + eighteenZeros));
   });
 });
