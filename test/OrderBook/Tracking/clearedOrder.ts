551f56125ff12baa8199382cadcb2035eafd62b2
marcusnewton <marcus.newton@hey.com>
2022-11-04 18:35:55 +1100

    debug cleared order

--- test/OrderBook/Tracking/clearedOrder.ts
+++ test/OrderBook/Tracking/clearedOrder.ts
@@ -27,6 +27,7 @@ import { rainterpreterDeploy } from "../../../utils/deploy/interpreter/shared/ra
 import { rainterpreterExpressionDeployer } from "../../../utils/deploy/interpreter/shared/rainterpreterExpressionDeployer/deploy";
 import { getEventArgs } from "../../../utils/events";
 import {
+  Debug,
   memoryOperand,
   MemoryType,
   op,
@@ -86,6 +87,22 @@ describe("OrderBook tracking order funds cleared", async function () {
     const v5 = op(Opcode.STATE, memoryOperand(MemoryType.Constant, 2));
     // prettier-ignore
     const askSource = concat([
+        op(Opcode.CALLER),
+        cOrderHash,
+      op(Opcode.IORDERBOOKV1_CLEARED_ORDER),
+
+             op(Opcode.BLOCK_NUMBER),
+            vAskBlock,
+          op(Opcode.SUB, 2),
+          v5,
+        op(Opcode.MUL, 2),
+          op(Opcode.CALLER),
+          cOrderHash,
+        op(Opcode.IORDERBOOKV1_CLEARED_ORDER),
+      op(Opcode.SUB, 2),
+
+      op(Opcode.DEBUG, Debug.StatePacked),
+
       // outputMax = (currentBlock - askBlock) * 5 - aliceCleared
       // 5 tokens available per block
             op(Opcode.BLOCK_NUMBER),
