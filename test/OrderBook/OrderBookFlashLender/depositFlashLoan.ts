a100c5df9f80033ea76508d2edf960c28756131c
thedavidmeister <thedavidmeister@gmail.com>
2022-11-14 16:52:59 +0400

    lint

--- test/OrderBook/OrderBookFlashLender/depositFlashLoan.ts
+++ test/OrderBook/OrderBookFlashLender/depositFlashLoan.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //// @ts-nocheck
 
-import { assert } from "chai";
 import { ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 import type {
