35609894a2df9b2f647a96e6b4b65c4619c99bb5
thedavidmeister <thedavidmeister@gmail.com>
2022-12-22 15:00:38 +0400

    fix tests

--- contracts/interpreter/ops/core/OpContext.sol
+++ contracts/interpreter/ops/core/OpContext.sol
@@ -45,13 +45,10 @@ library OpContext {
         InterpreterState memory state_,
         Operand operand_,
         StackTop stackTop_
-    ) internal pure returns (StackTop) {
+    ) internal view returns (StackTop) {
         // The indexing syntax here enforces OOB checks at runtime.
-        return
-            stackTop_.push(
-                state_.context[Operand.unwrap(operand_) >> 8][
-                    Operand.unwrap(operand_) & MASK_8BIT
-                ]
-            );
+        return stackTop_.push(state_.context[Operand.unwrap(operand_) >> 8][
+            Operand.unwrap(operand_) & MASK_8BIT
+        ]);
     }
 }
