4650bf77f1196fdd8358293c041aa32cdb42daa7
thedavidmeister <thedavidmeister@gmail.com>
2022-12-27 17:58:25 +0400

    docs

--- contracts/interpreter/run/IInterpreterV1.sol
+++ contracts/interpreter/run/IInterpreterV1.sol
@@ -6,9 +6,76 @@ type EncodedDispatch is uint256;
 type StateNamespace is uint256;
 type Operand is uint256;
 
+/// @title IInterpreterV1
+/// Interface into a standard interpreter that supports:
+///
+/// - evaluating `view` logic deployed onchain by an `IExpressionDeployerV1`
+/// - receiving arbitrary `uint256[][]` supporting context to be made available
+///   to the evaluated logic
+/// - handling subsequent state changes in bulk in response to evaluated logic
+/// - namespacing state changes according to the caller's preferences to avoid
+///   unwanted key collisions
+/// - exposing its internal function pointers to support external precompilation
+///   of logic for more gas efficient runtime evaluation by the interpreter
+///
+/// The interface is designed to be stable across many versions and
+/// implementations of an interpreter, balancing minimalism with features
+/// required for a general purpose onchain interpreted compute environment.
+///
+/// The security model of an interpreter is that it MUST be resilient to
+/// malicious expressions even if they dispatch arbitrary internal function
+/// pointers during an eval. The interpreter MAY return garbage or exhibit
+/// undefined behaviour or error during an eval, _provided that no state changes
+/// are persisted_ e.g. in storage, such that only the caller that specifies the
+/// malicious expression can be negatively impacted by the result. In turn, the
+/// caller must guard itself against arbitrarily corrupt/malicious reverts and
+/// return values from any interpreter that it requests an expression from. And
+/// so on and so forth up to the externally owned account (EOA) who signs the
+/// transaction and agrees to a specific combination of contracts, expressions
+/// and interpreters, who can presumably make an informed decision about which
+/// ones to trust to get the job done.
+///
+/// The state changes for an interpreter are expected to be produces by an `eval`
+/// and passed back to the interpreter as-is by the caller, after the caller has
+/// had an opportunity to apply their own intermediate logic such as reentrancy
+/// defenses against malicious interpreters. The interpreter is free to structure
+/// the state changes however it wants but MUST guard against the calling
+/// contract corrupting the changes between `eval` and `stateChanges`. For
+/// example an interpreter could sandbox storage writes per-caller so that a
+/// malicious caller can only damage their own state changes, while honest
+/// callers respect, benefit from and are protected by the interpreter's state
+/// change handling.
 interface IInterpreterV1 {
+    /// Exposes the function pointers as `uint16` values packed into a single
+    /// `bytes` in the same order as they would be indexed into by opcodes. For
+    /// example, if opcode `2` should dispatch function at position `0x1234` then
+    /// the start of the returned bytes would be `0xXXXXXXXX1234` where `X` is
+    /// a placeholder for the function pointers of opcodes `0` and `1`.
+    ///
+    /// `IExpressionDeployerV1` contracts use these function pointers to
+    /// "compile" the expression into something that an interpreter can dispatch
+    /// directly without paying gas to lookup the same at runtime. As the
+    /// validity of any integrity check and subsequent dispatch is highly
+    /// sensitive to both the function pointers and overall bytecode of the
+    /// interpreter, `IExpressionDeployerV1` contracts SHOULD implement guards
+    /// against accidentally being deployed onchain paired against an unknown
+    /// interpreter. It is very easy for an apparent compatible pairing to be
+    /// subtly and critically incompatible due to addition/removal/reordering of
+    /// opcodes and compiler optimisations on the interpreter bytecode.
+    ///
+    /// This MAY return different values during construction vs. all other times
+    /// after the interpreter has been successfully deployed onchain. DO NOT rely
+    /// on function pointers reported during contract construction.
     function functionPointers() external view returns (bytes memory);
 
+    /// The raison d'etre for an interpreter. Given some expression and per-call
+    /// additional contextual data, produce a stack of results and a set of state
+    /// changes that the caller MAY OPTIONALLY pass back to be persisted by a
+    /// call to `stateChanges`.
+    /// @param dispatch All the information required for the interpreter to load
+    /// an expression, select an entrypoint and return the values expected by the
+    /// caller. The interpreter MAY encode dispatches differently to
+    /// `LibEncodedDispatch` but this WILL negatively impact compatibility.
     function eval(
         EncodedDispatch dispatch,
         uint256[][] calldata context
@@ -19,6 +86,31 @@ interface IInterpreterV1 {
 
     function stateChanges(uint256[] calldata stateChanges) external;
 
+    /// Same as `eval` but allowing the caller to specify a namespace under which
+    /// the state changes will be applied. The interpeter MUST ensure that keys
+    /// will never collide across namespaces, even if, for example:
+    ///
+    /// - The calling contract is malicious and attempts to craft a collision
+    ///   with state changes from another contract
+    /// - The expression is malicious and attempts to craft a collision with
+    ///   other expressions evaluated by the same calling contract
+    ///
+    /// A malicious entity MAY have access to significant offchain resources to
+    /// attempt to precompute key collisions through brute force. The collision
+    /// resistance of namespaces should be comparable or equivalent to the
+    /// collision resistance of the hashing algorithms employed by the blockchain
+    /// itself, such as the design of `mapping` in Solidity that hashes each
+    /// nested key to produce a collision resistant compound key.
+    ///
+    /// Calls to `eval` without a namespace are implied to be under namespace `0`
+    /// so an interpreter MAY implement `eval` in terms of `evalWithNamespace` if
+    /// this simplifies the implementation.
+    ///
+    /// @param namespace The namespace specified by the calling contract.
+    /// @param dispatch As per `eval`.
+    /// @param context As per `eval`.
+    /// @return stack As per `eval`.
+    /// @return stateChanges As per `eval`.
     function evalWithNamespace(
         StateNamespace namespace,
         EncodedDispatch dispatch,
@@ -28,6 +120,11 @@ interface IInterpreterV1 {
         view
         returns (uint256[] memory stack, uint256[] memory stateChanges);
 
+    /// Same as `stateChanges` but following `evalWithNamespace`. The caller MUST
+    /// use the same namespace for both `evalWithNamespace` and
+    /// `stateChangesWithNamespace` for a given expression evaluation.
+    /// @param namespace As per `evalWithNamespace`.
+    /// @param stateChanges as per `stateChanges`.
     function stateChangesWithNamespace(
         StateNamespace namespace,
         uint256[] calldata stateChanges
