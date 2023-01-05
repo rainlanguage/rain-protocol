996440c701ff5f616b6467a39491c4d490a786d3
mutahhirkhan <mutahhir199@gmail.com>
2023-01-05 23:13:16 +0500

    adding withdraw reverting test scenario

--- utils/constants/stake.ts
+++ utils/constants/stake.ts
@@ -1,6 +1,31 @@
 import { ethers } from "hardhat";
 import { sixZeros } from ".";
+import { BigNumber } from "ethers";
+import { Stake } from "../../typechain";
 
 export const THRESHOLDS = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000].map(
   (value) => ethers.BigNumber.from(value + sixZeros)
 );
+
+interface Deposit {
+  timestamp: number;
+  amount: BigNumber;
+}
+
+export const getDeposits = async (stake: Stake, signerAddress: string) => {
+  const deposits: Deposit[] = [];
+  let i = 0;
+  let flag = true;
+
+  while (flag) {
+    try {
+      const depositItem = await stake.deposits(signerAddress, i);
+      deposits.push(depositItem);
+      i++;
+    } catch (e) {
+      flag = false;
+    }
+  }
+
+  return deposits;
+};
