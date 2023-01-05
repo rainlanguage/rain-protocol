8e3ed58026e91ffbe955a7e8520c27abd8cc1dbb
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-01-05 18:21:37 +0530

    Intialize Test added

---
+++ test/Lobby/intialize.ts
@@ -0,0 +1,108 @@
+import { assert } from "chai";
+import { arrayify, concat, solidityKeccak256 } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import type {
+  Rainterpreter,
+  RainterpreterExpressionDeployer,
+  ReserveToken18,
+} from "../../typechain";
+import {
+    InitializeEvent,
+  LobbyConfigStruct,
+} from "../../typechain/contracts/lobby/Lobby";
+import { assertError, compareStructs, fixedPointMul } from "../../utils";
+import { ONE, sixteenZeros } from "../../utils/constants/bigNumber";
+import { basicDeploy } from "../../utils/deploy/basicDeploy";
+import { rainterpreterDeploy } from "../../utils/deploy/interpreter/shared/rainterpreter/deploy";
+import { rainterpreterExpressionDeployerDeploy } from "../../utils/deploy/interpreter/shared/rainterpreterExpressionDeployer/deploy";
+import { getEventArgs } from "../../utils/events";
+import {
+  memoryOperand,
+  MemoryType,
+  op,
+} from "../../utils/interpreter/interpreter";
+import { RainterpreterOps } from "../../utils/interpreter/ops/allStandardOps";
+
+describe("Lobby Tests Intialize", async function () { 
+  const Opcode = RainterpreterOps;
+
+  let tokenA:ReserveToken18;
+  let interpreter: Rainterpreter;
+  let expressionDeployer: RainterpreterExpressionDeployer;
+
+
+  before(async () => {
+    interpreter = await rainterpreterDeploy();
+    expressionDeployer = await rainterpreterExpressionDeployerDeploy(
+      interpreter
+    );
+  });
+
+  beforeEach(async () => {
+    tokenA = (await basicDeploy("ReserveToken18", {})) as ReserveToken18;
+    await tokenA.initialize();
+  }); 
+
+  it("Lobby is intialized correctly", async function () { 
+    const signers = await ethers.getSigners(); 
+  
+    const timeoutDuration = 15000000
+    const Lobby = await basicDeploy("Lobby", {}, [timeoutDuration]);
+
+    const constants = [0,1,ONE];
+
+    // prettier-ignore
+    const joinSource = concat([
+        op(Opcode.CONTEXT, 0x0300) ,
+        op(Opcode.READ_MEMORY,memoryOperand(MemoryType.Constant, 2))
+      ]);
+
+    const leaveSource = concat([
+      op(Opcode.READ_MEMORY, memoryOperand(MemoryType.Constant, 2))
+    ]);
+    const claimSource = concat([
+        op(Opcode.CONTEXT, 0x0100)
+    ]);
+
+    const invalidSource = concat([
+      op(Opcode.READ_MEMORY, memoryOperand(MemoryType.Constant, 0)),
+    ]);
+
+    const lobbyStateConfig = {
+      sources: [joinSource, leaveSource, claimSource, invalidSource],
+      constants: constants,
+    };
+
+    const initialConfig: LobbyConfigStruct = {
+      refMustAgree: false,
+      ref: signers[0].address,
+      expressionDeployer: expressionDeployer.address,
+      interpreter: interpreter.address,
+      token: tokenA.address,
+      stateConfig: lobbyStateConfig,
+      description: [],
+      timeoutDuration: timeoutDuration,
+    };
+
+    const intializeTx = await Lobby.initialize(initialConfig);  
+
+    const intializeEvent = await (await getEventArgs(intializeTx, "Initialzie", Lobby)) as InitializeEvent["args"]; 
+
+    assert(intializeEvent.sender === signers[0].address, "wrong deposit sender")
+    compareStructs(intializeEvent.config,initialConfig)
+    
+
+  });   
+
+
+  
+
+
+
+
+  
+  
+
+
+
+});
