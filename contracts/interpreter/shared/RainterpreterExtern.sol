3b17f313c02bf247667513edddc5f63693fa2ec9
thedavidmeister <thedavidmeister@gmail.com>
2023-01-26 21:32:51 +0400

    add docs

--- contracts/interpreter/shared/RainterpreterExtern.sol
+++ contracts/interpreter/shared/RainterpreterExtern.sol
@@ -6,13 +6,18 @@ import "../ops/chainlink/OpChainlinkOraclePrice.sol";
 import "../run/LibStackPointer.sol";
 import "../../array/LibUint256Array.sol";
 
+/// Thrown when the inputs don't match the expected inputs.
 error BadInputs(uint256 expected, uint256 actual);
 
+/// EXPERIMENTAL implementation of `IInterpreterExternV1`.
+/// Currently only implements the Chainlink oracle price opcode as a starting
+/// point to test and flesh out externs generally.
 contract RainterpreterExtern is IInterpreterExternV1 {
     using LibStackPointer for uint256[];
     using LibStackPointer for StackPointer;
     using LibUint256Array for uint256;
 
+    /// @inheritdoc IInterpreterExternV1
     function extern(
         ExternDispatch,
         uint256[] memory inputs_
