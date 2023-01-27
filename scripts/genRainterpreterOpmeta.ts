ada856160a9926ad1470fc487efe9ffca25e75f0
rouzwelt <rouzweltt@gmail.com>
2023-01-27 02:03:41 +0000

    update scripts and nix commands

--- scripts/genRainterpreterOpmeta.ts
+++ scripts/genRainterpreterOpmeta.ts
@@ -54,6 +54,7 @@ const main = async() => {
     if (args.length === 1) {
         dir = path.resolve(root, args[0])
     }
+    else throw new Error("invalid number of arguments")
     if (!dir.endsWith(".json")) dir = dir + "/RainterpreterOpmeta.json"
 
     writeFile(dir, JSON.stringify(fileData, null, 4))
