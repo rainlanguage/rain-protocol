1d9a2b147efa6231058030a4faf0940a7c07b7cd
rouzwelt <rouzweltt@gmail.com>
2023-01-27 17:25:21 +0000

    update pometa formatting
    - scripts use prettier formatting

--- scripts/genOpmeta.ts
+++ scripts/genOpmeta.ts
@@ -3,102 +3,100 @@
 import Ajv from "ajv";
 import * as fs from "fs";
 import * as path from "path";
+import { format } from "prettier";
 import { argv } from "process";
 import { deflateSync } from "zlib";
 import OpmetaSchema from "../opmeta_schema.json";
 
 const readFile = (_path: string) => {
-    try {
-        return fs.readFileSync(_path).toString();
-    } catch (error) {
-        throw new Error(`invalid file, reason: ${error}`);
-    }
+  try {
+    return fs.readFileSync(_path).toString();
+  } catch (error) {
+    throw new Error(`invalid file, reason: ${error}`);
+  }
 };
 
 const writeFile = (_path: string, file: string) => {
-    try {
-        fs.writeFileSync(_path, file);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    fs.writeFileSync(_path, file);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const main = async() => {
-    const root = path.resolve()
-    const args = argv.slice(2)
-    let dir = root
-    let schemaPath = ""
-    for (let i = 0; i < args.length; i++) {
-        if (args[i].includes("path")) {
-            let _tmp = args.splice(i, i + 1)[0]
-            _tmp = _tmp.slice(_tmp.indexOf("=") + 1)
-            dir = path.resolve(root, _tmp)
-        }
+const main = async () => {
+  const root = path.resolve();
+  const args = argv.slice(2);
+  let dir = root;
+  let schemaPath = "";
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].includes("path")) {
+      let _tmp = args.splice(i, i + 1)[0];
+      _tmp = _tmp.slice(_tmp.indexOf("=") + 1);
+      dir = path.resolve(root, _tmp);
     }
-    for (let i = 0; i < args.length; i++) {
-        if (args[i].includes("schema")) {
-            let _tmp = args.splice(i, i + 1)[0]
-            _tmp = _tmp.slice(_tmp.indexOf("=") + 1)
-            if (_tmp.endsWith(".json")) {
-                schemaPath = path.resolve(root, _tmp)
-            }
-            else throw new Error("invalid schema path")
-        }
+  }
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].includes("schema")) {
+      let _tmp = args.splice(i, i + 1)[0];
+      _tmp = _tmp.slice(_tmp.indexOf("=") + 1);
+      if (_tmp.endsWith(".json")) {
+        schemaPath = path.resolve(root, _tmp);
+      } else throw new Error("invalid schema path");
     }
+  }
 
-    const schema = schemaPath.length 
-        ? JSON.parse(readFile(schemaPath)) 
-        : OpmetaSchema
-    const validate = (new Ajv()).compile(schema)
+  const schema = schemaPath.length
+    ? JSON.parse(readFile(schemaPath))
+    : OpmetaSchema;
+  const validate = new Ajv().compile(schema);
 
-    const opmetas = []
-    for (let i = 0; i < args.length; i++) {
-        if (args[i].endsWith(".json")) {
-            const tmp = JSON.parse(readFile(path.resolve(root, args[i])))
-            if (validate(tmp)) opmetas.push(tmp)
-            else throw new Error(`invalid opmeta content at index ${i}`)
-        }
-        else throw new Error(`invalid opmeta path at index ${i}`)
-    }
+  const opmetas = [];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].endsWith(".json")) {
+      const tmp = JSON.parse(readFile(path.resolve(root, args[i])));
+      if (validate(tmp)) opmetas.push(tmp);
+      else throw new Error(`invalid opmeta content at index ${i}`);
+    } else throw new Error(`invalid opmeta path at index ${i}`);
+  }
 
-    let opmetaHexString = "0x"
-    const opmetaBytes = Uint8Array.from(deflateSync(
-        JSON.stringify(opmetas, null, 4)
-    ))
-    for (let i = 0; i < opmetaBytes.length; i++) {
-        opmetaHexString = 
-            opmetaHexString + opmetaBytes[i].toString(16).padStart(2, "0")
-    }
+  let opmetaHexString = "0x";
+  const opmetaBytes = Uint8Array.from(
+    deflateSync(format(JSON.stringify(opmetas, null, 4), { parser: "json" }))
+  );
+  for (let i = 0; i < opmetaBytes.length; i++) {
+    opmetaHexString =
+      opmetaHexString + opmetaBytes[i].toString(16).padStart(2, "0");
+  }
 
-    let schemaHexString = "0x"
-    const schemaBytes = Uint8Array.from(deflateSync(
-        JSON.stringify(schema, null, 4)
-    ))
-    for (let i = 0; i < schemaBytes.length; i++) {
-        schemaHexString = 
-            schemaHexString + schemaBytes[i].toString(16).padStart(2, "0")
-    }
+  let schemaHexString = "0x";
+  const schemaBytes = Uint8Array.from(
+    deflateSync(format(JSON.stringify(schema, null, 4), { parser: "json" }))
+  );
+  for (let i = 0; i < schemaBytes.length; i++) {
+    schemaHexString =
+      schemaHexString + schemaBytes[i].toString(16).padStart(2, "0");
+  }
 
-    const fileData = {
-        opmeta: opmetas,
-        deployableOpmetaBytes: opmetaHexString,
-        deployableSchemaBytes: schemaHexString
-    }
+  const data = {
+    opmeta: opmetas,
+    deployableOpmetaBytes: opmetaHexString,
+    deployableSchemaBytes: schemaHexString,
+  };
+  const fileData = format(JSON.stringify(data, null, 4), { parser: "json" });
 
-    if (!dir.endsWith(".json")) dir = dir + "/Opmeta.json"
+  if (!dir.endsWith(".json")) dir = dir + "/Opmeta.json";
 
-    writeFile(dir, JSON.stringify(fileData, null, 4))
-}
+  writeFile(dir, fileData);
+};
 
-main().then(
-    () => {
-        const exit = process.exit;
-        exit(0);
-    }
-).catch(
-    (error) => {
-        console.error(error);
-        const exit = process.exit;
-        exit(1);
-    }
-);
+main()
+  .then(() => {
+    const exit = process.exit;
+    exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    const exit = process.exit;
+    exit(1);
+  });
--- scripts/genRainterpreterOpmeta.ts
+++ scripts/genRainterpreterOpmeta.ts
@@ -4,6 +4,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { argv } from "process";
 import { deflateSync } from "zlib";
+import { format } from "prettier";
 import OpmetaSchema from "../opmeta_schema.json";
 import { rainterpreterOpmeta } from "../utils/interpreter/ops/allStandardOpmeta";
 
@@ -16,59 +17,57 @@ import { rainterpreterOpmeta } from "../utils/interpreter/ops/allStandardOpmeta"
 // };
 
 const writeFile = (_path: string, file: string) => {
-    try {
-        fs.writeFileSync(_path, file);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    fs.writeFileSync(_path, file);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const main = async() => {
-    let opmetaHexString = "0x"
-    const opmetaBytes = Uint8Array.from(deflateSync(
-        JSON.stringify(rainterpreterOpmeta, null, 4)
-    ))
-    for (let i = 0; i < opmetaBytes.length; i++) {
-        opmetaHexString = 
-            opmetaHexString + opmetaBytes[i].toString(16).padStart(2, "0")
-    }
+const main = async () => {
+  let opmetaHexString = "0x";
+  const opmetaBytes = Uint8Array.from(
+    deflateSync(JSON.stringify(rainterpreterOpmeta, null, 4))
+  );
+  for (let i = 0; i < opmetaBytes.length; i++) {
+    opmetaHexString =
+      opmetaHexString + opmetaBytes[i].toString(16).padStart(2, "0");
+  }
 
-    let schemaHexString = "0x"
-    const schemaBytes = Uint8Array.from(deflateSync(
-        JSON.stringify(OpmetaSchema, null, 4)
-    ))
-    for (let i = 0; i < schemaBytes.length; i++) {
-        schemaHexString = 
-            schemaHexString + schemaBytes[i].toString(16).padStart(2, "0")
-    }
+  let schemaHexString = "0x";
+  const schemaBytes = Uint8Array.from(
+    deflateSync(JSON.stringify(OpmetaSchema, null, 4))
+  );
+  for (let i = 0; i < schemaBytes.length; i++) {
+    schemaHexString =
+      schemaHexString + schemaBytes[i].toString(16).padStart(2, "0");
+  }
 
-    const fileData = {
-        opmeta: rainterpreterOpmeta,
-        deployableOpmetaBytes: opmetaHexString,
-        deployableSchemaBytes: schemaHexString
-    }
+  const data = {
+    opmeta: rainterpreterOpmeta,
+    deployableOpmetaBytes: opmetaHexString,
+    deployableSchemaBytes: schemaHexString,
+  };
+  const fileData = format(JSON.stringify(data, null, 4), { parser: "json" });
 
-    const root = path.resolve()
-    let dir = root
-    const args = argv.slice(2)
-    if (args.length === 1) {
-        dir = path.resolve(root, args[0])
-    }
-    else throw new Error("invalid number of arguments")
-    if (!dir.endsWith(".json")) dir = dir + "/RainterpreterOpmeta.json"
+  const root = path.resolve();
+  let dir = root;
+  const args = argv.slice(2);
+  if (args.length === 1) {
+    dir = path.resolve(root, args[0]);
+  } else if (args.length > 1) throw new Error("invalid number of arguments");
+  if (!dir.endsWith(".json")) dir = dir + "/RainterpreterOpmeta.json";
 
-    writeFile(dir, JSON.stringify(fileData, null, 4))
-}
+  writeFile(dir, fileData);
+};
 
-main().then(
-    () => {
-        const exit = process.exit;
-        exit(0);
-    }
-).catch(
-    (error) => {
-        console.error(error);
-        const exit = process.exit;
-        exit(1);
-    }
-);
+main()
+  .then(() => {
+    const exit = process.exit;
+    exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    const exit = process.exit;
+    exit(1);
+  });
