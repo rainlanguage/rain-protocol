76ab6d6ba15c4e086768b03b07ac00574ca7e011
thedavidmeister <thedavidmeister@gmail.com>
2023-01-31 10:04:45 +0400

    fix ops length

--- contracts/interpreter/ops/AllStandardOps.sol
+++ contracts/interpreter/ops/AllStandardOps.sol
@@ -71,7 +71,7 @@ import "./tier/OpUpdateTimesForTierRange.sol";
 error BadDynamicLength(uint256 dynamicLength, uint256 standardOpsLength);
 
 /// @dev Number of ops currently provided by `AllStandardOps`.
-uint256 constant ALL_STANDARD_OPS_LENGTH = 60;
+uint256 constant ALL_STANDARD_OPS_LENGTH = 61;
 
 /// @title AllStandardOps
 /// @notice Every opcode available from the core repository laid out as a single
