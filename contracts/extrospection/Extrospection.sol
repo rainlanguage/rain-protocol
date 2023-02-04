6521b961911cc7c0956eefff3bb47d3445c9699b
thedavidmeister <thedavidmeister@gmail.com>
2023-02-04 17:24:56 +0400

    emitBytecodeHash in extrospection

--- contracts/extrospection/Extrospection.sol
+++ contracts/extrospection/Extrospection.sol
@@ -5,17 +5,17 @@ pragma solidity =0.8.17;
 /// @notice Exposes certain information available to evm opcodes as public
 /// functions that are world callable.
 contract Extrospection {
-    event BytecodeHash(address sender, bytes32 hash);
+    event BytecodeHash(address sender, address account, bytes32 bytecodeHash);
 
-    function bytecodeHash(address address_) public view returns (bytes32) {
+    function bytecodeHash(address account_) public view returns (bytes32) {
         bytes32 hash_;
         assembly ("memory-safe") {
-            hash_ := extcodehash(address_)
+            hash_ := extcodehash(account_)
         }
         return hash_;
     }
 
-    function emitBytecodeHash(address address_) external {
-        emit BytecodeHash(msg.sender, bytecodeHash(address_));
+    function emitBytecodeHash(address account_) external {
+        emit BytecodeHash(msg.sender, account_, bytecodeHash(account_));
     }
 }
