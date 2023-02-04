fe4f1793d0921fc2b930576b90350f338e20306f
thedavidmeister <thedavidmeister@gmail.com>
2023-02-04 17:21:59 +0400

    add emit bytecodehash

--- contracts/extrospection/Extrospection.sol
+++ contracts/extrospection/Extrospection.sol
@@ -5,11 +5,17 @@ pragma solidity =0.8.17;
 /// @notice Exposes certain information available to evm opcodes as public
 /// functions that are world callable.
 contract Extrospection {
+    event BytecodeHash(address sender, bytes32 hash);
+
     function bytecodeHash(address address_) public view returns (bytes32) {
         bytes32 hash_;
         assembly ("memory-safe") {
             hash_ := extcodehash(address_)
         }
         return hash_;
+    }
+
+    function emitBytecodeHash(address address_) external {
+        emit BytecodeHash(msg.sender, bytecodeHash(address_));
     }
 }
