255e50e3e32c1f5acd3a85fbafda2ee57c0a6130
thedavidmeister <thedavidmeister@gmail.com>
2023-02-07 13:28:18 +0400

    add erc165 checker to extrospection

--- contracts/extrospection/Extrospection.sol
+++ contracts/extrospection/Extrospection.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: CAL
 pragma solidity =0.8.17;
 
+import {ERC165CheckerUpgradeable as ERC165Checker} from "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165CheckerUpgradeable.sol";
+
 /// @title Extrospection
 /// @notice Exposes certain information available to evm opcodes as public
 /// functions that are world callable.
 contract Extrospection {
     event BytecodeHash(address sender, address account, bytes32 bytecodeHash);
+
+    event SupportsInterface(
+        address sender,
+        address account,
+        bytes4 interfaceId,
+        bool supportsInterface
+    );
 
     function bytecodeHash(address account_) public view returns (bytes32) {
         bytes32 hash_;
@@ -17,5 +26,17 @@ contract Extrospection {
 
     function emitBytecodeHash(address account_) external {
         emit BytecodeHash(msg.sender, account_, bytecodeHash(account_));
+    }
+
+    function emitSupportsInterface(
+        address account_,
+        bytes4 interfaceId_
+    ) external {
+        emit SupportsInterface(
+            msg.sender,
+            account_,
+            interfaceId_,
+            ERC165Checker.supportsInterface(account_, interfaceId_)
+        );
     }
 }
