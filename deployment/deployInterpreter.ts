098eae0d77b766858a460bb927a28fb05aaa5d46
NanezX <victor.nanezj@gmail.com>
2023-02-12 22:38:20 -0400

    update ci deploy to use the keyless method deploy

--- deployment/deployInterpreter.ts
+++ deployment/deployInterpreter.ts
@@ -1,10 +1,7 @@
 import { ethers } from "hardhat";
 import deploy1820 from "../utils/deploy/registry1820/deploy";
-import {
-  rainterpreterDeploy,
-  rainterpreterStoreDeploy,
-} from "../utils/deploy/interpreter/shared/rainterpreter/deploy";
-import { rainterpreterExpressionDeployerDeploy } from "../utils/deploy/interpreter/shared/rainterpreterExpressionDeployer/deploy";
+
+import { getRainterpreterOpMetaBytes, keylessDeploy } from "../utils";
 
 const main = async function () {
   const [signer] = await ethers.getSigners();
@@ -12,15 +9,28 @@ const main = async function () {
   // This will deploy the registry only if does not exist in the network (like localhost/hardhat)
   await deploy1820(signer);
 
-  const interpreter = await rainterpreterDeploy();
-  const store = await rainterpreterStoreDeploy();
+  // Rainterpreter
+  const interpreter = await keylessDeploy("Rainterpreter", signer);
 
-  const deployer = await rainterpreterExpressionDeployerDeploy(
-    interpreter,
-    store
+  // RainterpreterStore
+  const store = await keylessDeploy("RainterpreterStore", signer);
+
+  // RainterpreterExpressionDeployer
+  const bytes_ = getRainterpreterOpMetaBytes();
+
+  const args = {
+    interpreter: interpreter.address,
+    store: store.address,
+    opMeta: bytes_,
+  };
+
+  const expressionDeployer = await keylessDeploy(
+    "RainterpreterExpressionDeployer",
+    signer,
+    args
   );
 
-  console.log("ExpressionDeployer deployed at: ", deployer.address);
+  console.log("ExpressionDeployer deployed at: ", expressionDeployer.address);
 };
 
 main()
