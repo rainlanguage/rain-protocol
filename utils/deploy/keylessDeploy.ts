2bf46e9372fae39718d7821657948b6ef1e042a6
NanezX <victor.nanezj@gmail.com>
2023-02-13 00:24:44 -0400

    updating to transaction type 2 EIP-1559

--- utils/deploy/keylessDeploy.ts
+++ utils/deploy/keylessDeploy.ts
@@ -1,6 +1,14 @@
 import { ethers } from "hardhat";
-import { Signer, utils } from "ethers";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { type Signer, utils, BigNumber } from "ethers";
+import { type SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import {
+  allChains,
+  configureChains,
+  createClient,
+  fetchFeeData,
+} from "@sonicswap/wagmi-core";
+import { publicProvider } from "@sonicswap/wagmi-core/providers/public";
 
 export const keylessDeploy = async (
   contractName: string,
@@ -10,22 +18,25 @@ export const keylessDeploy = async (
   args = Array.isArray(args) ? args : [args];
 
   const provider = signer.provider;
-
   if (!provider || !provider._isProvider) throw new Error("Not provider");
 
-  const factory = await ethers.getContractFactory(contractName);
+  const { chainId } = await provider.getNetwork();
+  const { maxFeePerGas, maxPriorityFeePerGas } = await estimateFeeData(chainId);
 
+  const factory = await ethers.getContractFactory(contractName);
   const txReq = factory.getDeployTransaction(...args);
 
   const gasLimit = await provider.estimateGas(txReq);
 
   const tx = {
     nonce: 0,
-    gasPrice: "0x09184e72a000",
-    gasLimit: gasLimit,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    gasLimit,
     value: "0x00",
     data: txReq.data,
-    type: 0,
+    type: 2,
+    chainId,
   };
 
   const signature = {
@@ -48,11 +59,15 @@ export const keylessDeploy = async (
   const code = await provider.getCode(contractAddress);
   if (code != "0x") return factory.attach(contractAddress);
 
+  const value = ethers.BigNumber.from(tx.gasLimit).mul(
+    tx.maxFeePerGas.add(tx.maxPriorityFeePerGas)
+  );
+
   // Send funds to the address
   await signer.sendTransaction({
     to: deployerAddress,
     // Send GasLimit * GasPrice of the tx
-    value: ethers.BigNumber.from(tx.gasLimit).mul(tx.gasPrice),
+    value,
   });
 
   const txResp = await provider.sendTransaction(rawTx);
@@ -64,4 +79,41 @@ export const keylessDeploy = async (
   contract.deployTransaction = txResp;
 
   return contract;
+};
+
+const estimateFeeData = async (
+  chainId: number
+): Promise<{
+  gasPrice: BigNumber;
+  maxFeePerGas: BigNumber;
+  maxPriorityFeePerGas: BigNumber;
+}> => {
+  if (chainId === 31337) {
+    return {
+      gasPrice: BigNumber.from("1980000104"),
+      maxFeePerGas: BigNumber.from("1500000030"),
+      maxPriorityFeePerGas: BigNumber.from("1500000000"),
+    };
+  } else {
+    const chain = allChains.find((chain) => chain.id === chainId);
+
+    const { provider, webSocketProvider } = configureChains(
+      [chain],
+      [publicProvider()]
+    );
+
+    createClient({
+      autoConnect: true,
+      provider,
+      webSocketProvider,
+    });
+    const { gasPrice, maxFeePerGas, maxPriorityFeePerGas } =
+      await fetchFeeData();
+
+    return {
+      gasPrice,
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+    };
+  }
 };
