75a4a940a8eabdd6ea7af3100443482a9b3fa2ed
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-02-21 11:16:21 +0530

    lint

--- test/Interpreter/Ops/Bytes32/decode.ts
+++ test/Interpreter/Ops/Bytes32/decode.ts
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import { ethers } from "hardhat";
 import { IInterpreterV1Consumer, Rainterpreter } from "../../../../typechain";
-import { assertError,  standardEvaluableConfig } from "../../../../utils";
+import { assertError, standardEvaluableConfig } from "../../../../utils";
 
 import { rainterpreterDeploy } from "../../../../utils/deploy/interpreter/shared/rainterpreter/deploy";
 import deploy1820 from "../../../../utils/deploy/registry1820/deploy";
@@ -10,7 +10,6 @@ import { expressionConsumerDeploy } from "../../../../utils/deploy/test/iinterpr
 describe("Decode Op Tests", async function () {
   let rainInterpreter: Rainterpreter;
   let logic: IInterpreterV1Consumer;
-
 
   before(async () => {
     // Deploy ERC1820Registry
@@ -27,17 +26,17 @@ describe("Decode Op Tests", async function () {
   });
 
   it("should decode correctly for given bit mask", async () => {
+    const source =
+      "0x000000000000000000000000000000000000000000000000000000000000ffff";
+    const expected =
+      "0xffff00000000000000000000000000000000000000000000000000000000cccc";
 
-    const source = '0x000000000000000000000000000000000000000000000000000000000000ffff'
-    const expected = '0xffff00000000000000000000000000000000000000000000000000000000cccc'
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: decode-256<240 16>(${expected});`);
 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: decode-256<240 16>(${expected});`
-    );
-    
     const expression0 = await expressionConsumerDeploy(
-       sources0,
-       constants0,
+      sources0,
+      constants0,
       rainInterpreter,
       1
     );
@@ -48,25 +47,26 @@ describe("Decode Op Tests", async function () {
       []
     );
 
-    const result0 = await logic.stackTop() 
-    assert(ethers.utils.hexStripZeros(result0.toHexString()) == ethers.utils.hexStripZeros(source) , "Incorrectly decoded target")
-
-    
-
-  });  
+    const result0 = await logic.stackTop();
+    assert(
+      ethers.utils.hexStripZeros(result0.toHexString()) ==
+        ethers.utils.hexStripZeros(source),
+      "Incorrectly decoded target"
+    );
+  });
 
   it("should encode with bit mask of length 32 ", async () => {
+    const source =
+      "0x00000000000000000000000000000000cccccccccccccccccccccccccccccccc";
+    const expected =
+      "0xccccccccccccccccccccccccccccccccffffffffffffffffffffffffffffffff";
 
-    const source = '0x00000000000000000000000000000000cccccccccccccccccccccccccccccccc'
-    const expected = '0xccccccccccccccccccccccccccccccccffffffffffffffffffffffffffffffff'
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: decode-256<128 128>(${expected});`);
 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: decode-256<128 128>(${expected});`
-    );
-    
     const expression0 = await expressionConsumerDeploy(
-       sources0,
-       constants0,
+      sources0,
+      constants0,
       rainInterpreter,
       1
     );
@@ -77,89 +77,88 @@ describe("Decode Op Tests", async function () {
       []
     );
 
-    const result0 = await logic.stackTop()
-    assert(ethers.utils.hexStripZeros(result0.toHexString()) == ethers.utils.hexStripZeros(source) , "Incorrectly decoded target")
-
-
-  });  
+    const result0 = await logic.stackTop();
+    assert(
+      ethers.utils.hexStripZeros(result0.toHexString()) ==
+        ethers.utils.hexStripZeros(source),
+      "Incorrectly decoded target"
+    );
+  });
 
   it("should fail if (startBit + length) exceeds 256 bits", async () => {
+    const expected =
+      "0xcccccccccccccccccccccccccccccccccfffffffffffffffffffffffffffffff";
 
-    const source =     '0x0000000000000000000000000000000ccccccccccccccccccccccccccccccccc' 
-    const expected =   '0xcccccccccccccccccccccccccccccccccfffffffffffffffffffffffffffffff'
+    // startBit + length exceeds 256 bits
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: decode-256<128 129>(${expected});`);
 
-    // startBit + length exceeds 256 bits 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: decode-256<128 129>(${expected});`
-    );
-    
     await assertError(
       async () =>
-      await expressionConsumerDeploy(
-        sources0,
-        constants0,
-       rainInterpreter,
-       1
-     ),
+        await expressionConsumerDeploy(
+          sources0,
+          constants0,
+          rainInterpreter,
+          1
+        ),
       "TruncatedEncoding",
       "Incorrect decoded bits"
     );
-
-  });  
+  });
 
   it("should encode-decode for a round trip", async () => {
+    const source =
+      "0x0000000000000000000000000000000000000000cccccccc00000000dddddddd";
+    const target =
+      "0xaaaaaaaa00000000bbbbbbbb0000000000000000000000000000000000000000";
+    const expected =
+      "0xaaaaaaaa00000000bbbbbbbb0000000000000000cccccccc00000000dddddddd";
 
-    const source =     '0x0000000000000000000000000000000000000000cccccccc00000000dddddddd'
-    const target =     '0xaaaaaaaa00000000bbbbbbbb0000000000000000000000000000000000000000' 
-    const expected =   '0xaaaaaaaa00000000bbbbbbbb0000000000000000cccccccc00000000dddddddd'
-
-
-    // startBit + length exceeds 256 bits 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: encode-256<0 96>(${source} ${target});`
-    );
+    // startBit + length exceeds 256 bits
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: encode-256<0 96>(${source} ${target});`);
 
     const expression0 = await expressionConsumerDeploy(
-        sources0,
-        constants0,
-       rainInterpreter,
-       1
-     );
- 
-     await logic["eval(address,uint256,uint256[][])"](
-       rainInterpreter.address,
-       expression0.dispatch,
-       []
-     );
- 
-     const result0 = await logic.stackTop() 
-     assert(ethers.utils.hexStripZeros(result0.toHexString()) == ethers.utils.hexStripZeros(expected) , "Incorrect Encoding") 
+      sources0,
+      constants0,
+      rainInterpreter,
+      1
+    );
 
-     const { sources: sources1, constants: constants1 } = standardEvaluableConfig(
-        `_: decode-256<0 96>(${expected});`
-      );
-      
-      const expression1= await expressionConsumerDeploy(
-         sources1,
-         constants1,
-        rainInterpreter,
-        1
-      );
-  
-      await logic["eval(address,uint256,uint256[][])"](
-        rainInterpreter.address,
-        expression1.dispatch,
-        []
-      );
-  
-      const result1 = await logic.stackTop() 
-      assert(ethers.utils.hexStripZeros(result1.toHexString()) == ethers.utils.hexStripZeros(source) , "Incorrectly decoded target")
-    
-    
+    await logic["eval(address,uint256,uint256[][])"](
+      rainInterpreter.address,
+      expression0.dispatch,
+      []
+    );
 
-  }); 
+    const result0 = await logic.stackTop();
+    assert(
+      ethers.utils.hexStripZeros(result0.toHexString()) ==
+        ethers.utils.hexStripZeros(expected),
+      "Incorrect Encoding"
+    );
 
+    const { sources: sources1, constants: constants1 } =
+      standardEvaluableConfig(`_: decode-256<0 96>(${expected});`);
 
-  
+    const expression1 = await expressionConsumerDeploy(
+      sources1,
+      constants1,
+      rainInterpreter,
+      1
+    );
+
+    await logic["eval(address,uint256,uint256[][])"](
+      rainInterpreter.address,
+      expression1.dispatch,
+      []
+    );
+
+    const result1 = await logic.stackTop();
+    assert(
+      ethers.utils.hexStripZeros(result1.toHexString()) ==
+        ethers.utils.hexStripZeros(source),
+      "Incorrectly decoded target"
+    );
+  });
 });
- 
--- test/Interpreter/Ops/Bytes32/encode.ts
+++ test/Interpreter/Ops/Bytes32/encode.ts
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import { ethers } from "hardhat";
 import { IInterpreterV1Consumer, Rainterpreter } from "../../../../typechain";
-import { assertError,  standardEvaluableConfig } from "../../../../utils";
+import { assertError, standardEvaluableConfig } from "../../../../utils";
 
 import { rainterpreterDeploy } from "../../../../utils/deploy/interpreter/shared/rainterpreter/deploy";
 import deploy1820 from "../../../../utils/deploy/registry1820/deploy";
@@ -10,7 +10,6 @@ import { expressionConsumerDeploy } from "../../../../utils/deploy/test/iinterpr
 describe("Encode Op Tests", async function () {
   let rainInterpreter: Rainterpreter;
   let logic: IInterpreterV1Consumer;
-
 
   before(async () => {
     // Deploy ERC1820Registry
@@ -27,15 +26,16 @@ describe("Encode Op Tests", async function () {
   });
 
   it("should encode with bit mask of length 4", async () => {
+    const source =
+      "0x000000000000000000000000000000000000000000000000000000000000ffff";
+    const target =
+      "0x000000000000000000000000000000000000000000000000000000000000cccc";
+    const expected =
+      "0x0000000000000000000000000000000000000000000000000000000ffffcccc";
 
-    const source = '0x000000000000000000000000000000000000000000000000000000000000ffff'
-    const target = '0x000000000000000000000000000000000000000000000000000000000000cccc'
-    const expected = '0x0000000000000000000000000000000000000000000000000000000ffffcccc'
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: encode-256<16 16>(${source} ${target});`);
 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: encode-256<16 16>(${source} ${target});`
-    );
-    
     const expression0 = await expressionConsumerDeploy(
       sources0,
       constants0,
@@ -49,27 +49,29 @@ describe("Encode Op Tests", async function () {
       []
     );
 
-    const result0 = await logic.stackTop() 
-   
-    assert(ethers.utils.hexStripZeros(result0.toHexString()) == ethers.utils.hexStripZeros(expected) , "Incorrect Encoding")
+    const result0 = await logic.stackTop();
 
-    
-
-  });  
+    assert(
+      ethers.utils.hexStripZeros(result0.toHexString()) ==
+        ethers.utils.hexStripZeros(expected),
+      "Incorrect Encoding"
+    );
+  });
 
   it("should encode with bit mask of length 32 ", async () => {
+    const source =
+      "0x00000000000000000000000000000000cccccccccccccccccccccccccccccccc";
+    const target =
+      "0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff";
+    const expected =
+      "0xccccccccccccccccccccccccccccccccffffffffffffffffffffffffffffffff";
 
-    const source =   '0x00000000000000000000000000000000cccccccccccccccccccccccccccccccc'
-    const target =   '0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff'
-    const expected = '0xccccccccccccccccccccccccccccccccffffffffffffffffffffffffffffffff'
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: encode-256<128 128>(${source} ${target});`);
 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: encode-256<128 128>(${source} ${target});`
-    );
-    
     const expression0 = await expressionConsumerDeploy(
-       sources0,
-       constants0,
+      sources0,
+      constants0,
       rainInterpreter,
       1
     );
@@ -80,36 +82,34 @@ describe("Encode Op Tests", async function () {
       []
     );
 
-    const result0 = await logic.stackTop()
-    assert(ethers.utils.hexStripZeros(result0.toHexString()) == ethers.utils.hexStripZeros(expected) , "Incorrect Encoding")
-
-
-  });  
+    const result0 = await logic.stackTop();
+    assert(
+      ethers.utils.hexStripZeros(result0.toHexString()) ==
+        ethers.utils.hexStripZeros(expected),
+      "Incorrect Encoding"
+    );
+  });
 
   it("should fail if (startBit + length) exceeds 256 bits", async () => {
+    const source =
+      "0x0000000000000000000000000000000ccccccccccccccccccccccccccccccccc";
+    const target =
+      "0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff";
 
-    const source =   '0x0000000000000000000000000000000ccccccccccccccccccccccccccccccccc' 
-    const target =   '0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff'
+    // startBit + length exceeds 256 bits
+    const { sources: sources0, constants: constants0 } =
+      standardEvaluableConfig(`_: encode-256<128 129>(${source} ${target});`);
 
-    // startBit + length exceeds 256 bits 
-    const { sources: sources0, constants: constants0 } = standardEvaluableConfig(
-      `_: encode-256<128 129>(${source} ${target});`
-    );
-    
     await assertError(
       async () =>
-      await expressionConsumerDeploy(
-        sources0,
-        constants0,
-       rainInterpreter,
-       1
-     ),
+        await expressionConsumerDeploy(
+          sources0,
+          constants0,
+          rainInterpreter,
+          1
+        ),
       "TruncatedEncoding",
       "Incorrect encoded bits"
     );
-
-  }); 
-
-
-  
+  });
 });
