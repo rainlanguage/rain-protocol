0a8a19ad12a4268dcc1b13841235fd2eeb6ff045
thedavidmeister <thedavidmeister@gmail.com>
2023-02-28 10:25:08 +0400

    slither

--- contracts/stake/Stake.sol
+++ contracts/stake/Stake.sol
@@ -544,11 +544,11 @@ contract Stake is
             uint256 len_ = depositRecords[account_].length;
             high_ = len_;
             uint256 mid_;
-            DepositRecord memory depositRecord_;
+            DepositRecord memory record_;
             while (low_ < high_) {
                 mid_ = Math.average(low_, high_);
-                depositRecord_ = depositRecords[account_][mid_];
-                if (uint256(depositRecord_.amount) >= threshold_) {
+                record_ = depositRecords[account_][mid_];
+                if (uint256(record_.amount) >= threshold_) {
                     high_ = mid_;
                 } else {
                     low_ = mid_ + 1;
