3b62469c2f5b71abd46807f7c07b55f8038692d9
thedavidmeister <thedavidmeister@gmail.com>
2023-03-08 17:37:37 +0400

    hash

--- contracts/interpreter/shared/RainterpreterExpressionDeployer.sol
+++ contracts/interpreter/shared/RainterpreterExpressionDeployer.sol
@@ -42,7 +42,7 @@ bytes constant OPCODE_FUNCTION_POINTERS = hex"0b3f0b4e0b5d0be00bee0c400cb00d2e0d
 
 /// @dev Hash of the known interpreter bytecode.
 bytes32 constant INTERPRETER_BYTECODE_HASH = bytes32(
-    0xbd6cdaee09f8333c706bcb31204f8c4a0b58850f519013eea18afd6c29d5d43a
+    0x5b05e228776518e6b3c289e9462b8bd8ba2e0222230885d6e1e0a59d66b03340
 );
 
 /// @dev Hash of the known store bytecode.
