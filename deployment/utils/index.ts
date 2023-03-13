cdeea63f2dd601c935bc1218438deec2dc6ec76f
NanezX <victor.nanezj@gmail.com>
2023-03-12 23:28:52 -0400

    Single printer address

--- deployment/utils/index.ts
+++ deployment/utils/index.ts
@@ -6,8 +6,13 @@ const Register: AddressPrint = {};
 
 export function registerContract(name_: string, address_: string): void {
   Register[name_] = address_;
+  printAddress(name_, address_);
 }
 
-export function printAddresses() {
+export function printAddress(name_: string, address_: string) {
+  console.log(`${name_} deployed at: ${address_}`);
+}
+
+export function printAllAddresses() {
   console.table(Register);
 }
