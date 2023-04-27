77390f7b86f0df6b481c2dea21b85ed26926914a
thedavidmeister <thedavidmeister@gmail.com>
2023-04-27 10:09:15 +0400

    introduce row offset for fold context

--- contracts/interpreter/ops/context/OpFoldContext.sol
+++ contracts/interpreter/ops/context/OpFoldContext.sol
@@ -30,9 +30,11 @@ library OpFoldContext {
         unchecked {
             uint256 sourceIndex_ = Operand.unwrap(operand_) & MASK_4BIT;
             // We don't use the column for anything in the integrity check.
-            // uint256 column_ = (Operand.unwrap(operand_) >> 4) & MASK_4BIT;
-            uint256 width_ = (Operand.unwrap(operand_) >> 8) & MASK_4BIT;
-            uint256 inputs_ = Operand.unwrap(operand_) >> 12;
+            // uint256 column_ = (Operand.unwrap(operand_) >> 4) & MASK_3BIT;
+            uint256 width_ = (Operand.unwrap(operand_) >> 7) & MASK_3BIT;
+            // We don't use the row offset for anything in the integrity check
+            // uint256 row_ = (Operand.unwrap(operand_) >> 10) & MASK_3BIT;
+            uint256 inputs_ = Operand.unwrap(operand_) >> 13;
             uint256 callInputs_ = width_ + inputs_;
 
             // Outputs for call is the same as the inputs.
@@ -61,16 +63,17 @@ library OpFoldContext {
     ) internal view returns (StackPointer) {
         unchecked {
             uint256 sourceIndex_ = Operand.unwrap(operand_) & MASK_4BIT;
-            uint256 column_ = (Operand.unwrap(operand_) >> 4) & MASK_4BIT;
-            uint256 width_ = (Operand.unwrap(operand_) >> 8) & MASK_4BIT;
-            uint256 inputs_ = Operand.unwrap(operand_) >> 12;
+            uint256 column_ = (Operand.unwrap(operand_) >> 4) & MASK_3BIT;
+            uint256 width_ = (Operand.unwrap(operand_) >> 7) & MASK_3BIT;
+            uint256 row_ = (Operand.unwrap(operand_) >> 10) & MASK_3BIT;
+            uint256 inputs_ = Operand.unwrap(operand_) >> 13;
             // Call will take the width of the context rows being copied and the
             // base inputs that will be the accumulators of the fold.
             uint256 callInputs_ = width_ + inputs_;
 
             // Fold over the entire context. This will error with an OOB index
             // if the context columns are not of the same length.
-            for (uint256 i_ = 0; i_ < state_.context[column_].length; i_++) {
+            for (uint256 i_ = row_; i_ < state_.context[column_].length; i_++) {
                 // Push the width of the context columns onto the stack as rows.
                 for (uint256 j_ = 0; j_ < width_; j_++) {
                     stackTop_ = stackTop_.push(
