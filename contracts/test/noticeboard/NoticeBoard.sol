df46cbfeae9fc40833012e7727d782e688d1feee
thedavidmeister <thedavidmeister@gmail.com>
2023-05-01 17:57:49 +0400

    bump sol version

--- contracts/test/noticeboard/NoticeBoard.sol
+++ contracts/test/noticeboard/NoticeBoard.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: CAL
-pragma solidity =0.8.18;
+pragma solidity =0.8.19;
 
 // import "sol.metadata/MetaBoard.sol";
--- contracts/test/testToken/ReserveTokenERC1155.sol
+++ contracts/test/testToken/ReserveTokenERC1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity =0.8.18;
+pragma solidity =0.8.19;
 
 import {ERC1155Upgradeable as ERC1155} from "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import {ERC1155BurnableUpgradeable as ERC1155Burnable} from "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
