c693e3dbfd161915ef8c6baeaf975a5df1785f10
thedavidmeister <thedavidmeister@gmail.com>
2023-05-16 17:11:01 +0400

    remove memory size

--- contracts/interpreter/shared/RainterpreterExpressionDeployer.sol
+++ contracts/interpreter/shared/RainterpreterExpressionDeployer.sol
@@ -46,7 +46,7 @@ bytes constant OPCODE_FUNCTION_POINTERS = hex"0afc0b130b220bb70bc50c170c870d050d
 
 /// @dev Hash of the known interpreter bytecode.
 bytes32 constant INTERPRETER_BYTECODE_HASH = bytes32(
-    0x4344f47f7828b55c173c07bb3f10a3d228f14fb424b9f68089748e121534b754
+    0x69a16e32e4391d10c1b263d172c50ff38f40d44f4108f647a6864330f950d1e9
 );
 
 /// @dev Hash of the known store bytecode.
