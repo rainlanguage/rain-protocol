280a1b57c1b4c0279980ce5df849594fcf9c5dee
thedavidmeister <thedavidmeister@gmail.com>
2023-07-13 18:18:45 +0400

    fix tests

---
+++ contracts/test/interpreter/ImportRainterpreterExtern.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.19;
+
+import {RainterpreterExtern} from "rain.interpreter/concrete/RainterpreterExtern.sol";
--- contracts/test/noticeboard/NoticeBoard.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: CAL
-pragma solidity =0.8.19;
-
-// import "sol.metadata/MetaBoard.sol";
